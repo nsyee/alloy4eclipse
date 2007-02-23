fa5d6e2bcaf5d3648daf4a56163ba3214b7acbd2
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-02-23 09:34:28 +0000

    Lets some package visible for others

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/ALSEditor.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/ALSEditor.java
@@ -1,6 +1,8 @@
 package fr.univartois.cril.alloyplugin.editor;
 
-
+import fr.univartois.cril.alloyplugin.launch.LaunchCompiler;
+import edu.mit.csail.sdg.alloy4compiler.ast.World;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ui.editors.text.TextEditor;
 
 /**
@@ -15,8 +17,9 @@ public class ALSEditor extends TextEditor {
 	protected void initializeEditor() {
 		super.initializeEditor();
 		// Attache la configuration
-		setSourceViewerConfiguration(new ALSSourceViewerConfiguration());
-		
-		
+		setSourceViewerConfiguration(new ALSSourceViewerConfiguration());	
+		LaunchCompiler.parser("");	
 	}
+	
+
 }
