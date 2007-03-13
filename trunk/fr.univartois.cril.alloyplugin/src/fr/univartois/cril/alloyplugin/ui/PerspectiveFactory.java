b2ee8c1a743bb47b6cb2353361273c0ac55f9a2b
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-13 18:05:16 +0000

    Display result in the command view. (only one text message for the moment)

--- trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/PerspectiveFactory.java
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/PerspectiveFactory.java
@@ -9,16 +9,10 @@ public class PerspectiveFactory implements IPerspectiveFactory {
 	public void createInitialLayout(IPageLayout layout) {
 
 		layout.addView("org.eclipse.ui.navigator.ProjectExplorer", IPageLayout.LEFT, (float) 0.2, layout.getEditorArea());
-		layout.addView("org.eclipse.ui.console.ConsoleView", IPageLayout.BOTTOM, (float) 0.75, layout.getEditorArea());
 		layout.addView("org.eclipse.ui.views.ContentOutline", IPageLayout.RIGHT, (float) 0.75, layout.getEditorArea());
-		//TODO find the ID commandview in the launch plugin (if found)
-	/*	layout.addView("fr.univartois.cril.alloyplugin.launch.views.AlloyCommandView", IPageLayout.RIGHT, (float) 0.8, layout.getEditorArea());
-		IViewLayout viewLayout = layout.getViewLayout("fr.univartois.cril.alloyplugin.launch.views.AlloyCommandView");
-		if (viewLayout!=null){
-			viewLayout.setCloseable(true);
-			viewLayout.setMoveable(true);
-			}	
-*/
+		layout.addView("org.eclipse.ui.console.ConsoleView", IPageLayout.BOTTOM, (float) 0.75, layout.getEditorArea());
+		
+		
 		
 	}
 
