0fdf18f134f33a0fafb6ec47a37c3f0cdaad817f
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-13 12:33:28 +0000

    Commands view calls Alloy parser only when it's been diplaying

--- fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/EditorListener.java
+++ fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/EditorListener.java
@@ -1,37 +1,48 @@
 package fr.univartois.cril.alloyplugin.launch;
 
+import org.eclipse.core.resources.IResource;
+
 import fr.univartois.cril.alloyplugin.launch.ui.AlloyCommandView;
 import fr.univartois.cril.alloyplugin.ui.IAlloyEditorListener;
 
 public class EditorListener implements IAlloyEditorListener {
 
-	public void fileChanged(String fileLocation) {
-	}
-	public void fileLoaded(String fileLocation) {
-		parseCommandsFor(fileLocation);
-	}
-	public void fileSaved(String fileLocation) {
-		parseCommandsFor(fileLocation);
 
+	public void fileLoaded(IResource resource) {
+		parseCommandsFor(resource);
+		
 	}
-	public void fileSetFocus(String fileLocation) {
-		displayCommandsFor(fileLocation);
+	
+	public void fileSaved(IResource resource) {
+		parseCommandsFor(resource);
+		
+	}
+	public void fileSetFocus(IResource resource) {
+		
+		displayCommandsFor(resource);
 	}
 
-	public void fileClosed(String fileLocation) {
+	public void fileChanged(IResource resource) {
+		// TODO Auto-generated method stub
+		
 	}
+	public void fileClosed(IResource resource) {
+		// TODO Auto-generated method stub	
+	}
+	
+	
 	/**
 	 * Displays all the commands of an als file. 
 	 */
-	private void displayCommandsFor(String fileLocation){		
-		AlloyCommandView.displayCommands(fileLocation);		
+	private void displayCommandsFor(IResource resource){		
+		AlloyCommandView.displayCommands(resource);		
 	}
 	/**
 	 * Launch Alloy parser for an als file. 
 	 */
-	private void parseCommandsFor(String fileLocation){		
-		AlloyCommandView.setCommands(
-				AlloyLaunching.launchParser(fileLocation),fileLocation);
-		displayCommandsFor(fileLocation);
+	private void parseCommandsFor(IResource resource){		
+		AlloyCommandView.setCommands(resource);
+		AlloyCommandView.forceDisplayCommands(resource);
 	}
+	
 }
--- fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/ui/ViewContentProvider.java
+++ fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/ui/ViewContentProvider.java
@@ -2,20 +2,23 @@ package fr.univartois.cril.alloyplugin.launch.ui;
 
 
 import java.util.HashMap;
+
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
+import fr.univartois.cril.alloyplugin.launch.AlloyLaunching;
 import fr.univartois.cril.alloyplugin.launch.ExecutableCommand;
 /**
  * Content provider for commands view.
  * */
 public class ViewContentProvider implements IStructuredContentProvider {
 	ExecutableCommand[] elements={};
-	HashMap<String, ExecutableCommand[]> map=new HashMap<String, ExecutableCommand[]>();
-	String currentId=null;
+	HashMap<IResource, ExecutableCommand[]> map=new HashMap<IResource, ExecutableCommand[]>();
+	IResource current=null;
 	public ViewContentProvider(){
 		//TODO fusionner avec l'editorListener?		
-		map.put(currentId, elements);
+		map.put(current, elements);
 	}		
 	public void inputChanged(Viewer v, Object oldInput, Object newInput) {
 		//System.out.println("v:"+v);
@@ -23,16 +26,74 @@ public class ViewContentProvider implements IStructuredContentProvider {
 		//System.out.println("newInput:"+newInput);
 	}			
 	public void dispose() {
+//		TODO implement this method
+	}
 
+	/**
+	 * Add a resource to the content provider.
+	 * If the AlloyCommandView is open, it call getCommandFromFile(resource)
+	 * and stores its commands.
+	 * If no, it associate a null value to the resource.
+	 */
+	public void addElements(IResource resource){
+		AlloyCommandView view = AlloyCommandView.getDefault();
+		ExecutableCommand[] elements=null;
+		System.out.print("ajoute");
+		if (view!=null) elements=getCommandFromFile(resource);
+		System.out.println(elements);
+		putElement(resource,elements);
+		
 	}
-	public void setElements(String filename,ExecutableCommand []elements){		
-		map.put(filename, elements);
+
+	private Object[] putElement(IResource resource, ExecutableCommand[] elements) {
+		return map.put(resource, elements);		
 	}
+	//TODO implement this method
+	public void removeElements(IResource resource){}
+	/**
+	 * Try to get the location of the resource. 
+	 * Returns the file location or null;
+	 * @param resource 
+	 */
+	//TODO move in a static util class?
+	private static String getFileLocation(IResource resource){		
+		if(resource!=null)
+			return resource.getLocation().toString();
+		else
+			return null;
+	}
+	/**
+	 * This method from IStructuredContentProvider is used by the viewer which this content provider is associed.
+	 * Returns all the commands for the current resource.
+	 */
 	public Object[] getElements(Object parent) {		
-		return map.get(currentId);					
+		ExecutableCommand[] exec_cmds = map.get(current);
+		System.out.println("get"+exec_cmds);
+		if (exec_cmds==null) 
+			{exec_cmds=getCommandFromFile(current);
+			putElement(current,exec_cmds);
+			return exec_cmds;}
+			
+		return exec_cmds;
 	}
-	public void setCurrentId(String id){
-		if(map.containsKey(id))
-			currentId=id;
+	/**
+	 * Launch the Alloy parser to get commands from the resource.
+	 */
+	private ExecutableCommand[] getCommandFromFile(IResource resource) {
+		System.out.println("launch:"+getFileLocation(resource));
+		return AlloyLaunching.launchParser(getFileLocation(resource));		
+	}
+	/**
+	 * Set the current resource for displaying its content (commands).
+	 */
+	public void setCurrent(IResource newResource){
+		if(map.containsKey(newResource))
+			current=newResource;
+	}
+	/**
+	 * I have no idea what this function does...
+	 */	  
+	public IResource getCurrent() {
+		return current;
 	};
 }
