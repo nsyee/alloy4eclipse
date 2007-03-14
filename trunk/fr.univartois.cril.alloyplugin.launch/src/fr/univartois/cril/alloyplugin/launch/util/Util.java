9f4bba3ccb4c9febea46df97e284bc74a1da2f68
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-14 17:53:31 +0000

    problems view: marker on the good resource

---
+++ trunk/fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/util/Util.java
@@ -0,0 +1,26 @@
+package fr.univartois.cril.alloyplugin.launch.util;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+
+public class Util {
+
+	/**
+	 * Try to get the location of the resource. 
+	 * Returns the file location or null;
+	 * @param resource 
+	 */
+	//TODO move in a static util class?
+	public static String getFileLocation(IResource resource){		
+		if(resource!=null)
+			return resource.getLocation().toString();
+		else
+			return null;
+	}
+	
+	public static IFile getFileForLocation(String filename){
+	return ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(new Path(filename));
+	}
+}
