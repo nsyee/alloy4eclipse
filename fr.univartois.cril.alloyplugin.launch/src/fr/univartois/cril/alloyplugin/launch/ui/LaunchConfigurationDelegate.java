493b99acb72cd843e7105c1df11af0b84e75283e
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-16 13:59:40 +0000

    modif launch config

---
+++ fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/ui/LaunchConfigurationDelegate.java
@@ -0,0 +1,16 @@
+package fr.univartois.cril.alloyplugin.launch.ui;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class LaunchConfigurationDelegate implements ILaunchConfigurationDelegate{
+
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		// TODO à implémenter
+		
+	}
+
+}
