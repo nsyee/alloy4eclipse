3ec011cf719fa0ab067c67f31c1ff3c29fed4ecb
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-20 14:26:59 +0000

    Added support for Markers Hover.

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/ALSSourceViewerConfiguration.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/ALSSourceViewerConfiguration.java
@@ -7,6 +7,8 @@ import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.BufferedRuleBasedScanner;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.Token;
+import org.eclipse.jface.text.source.DefaultAnnotationHover;
+import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
 
@@ -79,4 +81,10 @@ public class ALSSourceViewerConfiguration extends SourceViewerConfiguration {
 	
 		return reconciler;
 	}
+
+    @Override
+    public IAnnotationHover getAnnotationHover(ISourceViewer sourceViewer) {
+        return new DefaultAnnotationHover();
+    }
+    
 }
