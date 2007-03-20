eec9071009cdfc35136aba505650dbf3dd378b44
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-20 11:05:03 +0000

    delete unused classs, add comments ...

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/IAlloyEditorListener.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/IAlloyEditorListener.java
@@ -3,9 +3,24 @@ package fr.univartois.cril.alloyplugin.ui;
 import org.eclipse.core.resources.IResource;
 
 public interface IAlloyEditorListener {
-public void fileLoaded(IResource resource);	
-public void fileSaved(IResource resource);	
-public void fileChanged(IResource resource);
-public void fileClosed(IResource resource);
-public void fileSetFocus(IResource resource);
+	/**
+	 * when an als file is loaded. 
+	 */
+	public void fileLoaded(IResource resource);
+	/**
+	 * when an als file is saved. 
+	 */
+	public void fileSaved(IResource resource);
+	/**
+	 * when an als file change (not supported). 
+	 */
+	public void fileChanged(IResource resource);
+	/**
+	 * when an als file is closed. 
+	 */
+	public void fileClosed(IResource resource);
+	/**
+	 * when an als file has focus. 
+	 */
+	public void fileSetFocus(IResource resource);
 }
