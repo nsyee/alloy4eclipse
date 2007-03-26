8d228b2280b52935bc9edc28f5940ec013860fe9
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-26 21:09:30 +0000

    use IALSFile instead of IResource because when a Resource is renamed a new resource is created. so the hash map <IResource,...>  doesnt work when a als file is renammed

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/IAlloyEditorListener.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/IAlloyEditorListener.java
@@ -1,26 +1,27 @@
 package fr.univartois.cril.alloyplugin.ui;
-
-import org.eclipse.core.resources.IResource;
-
+/**
+ * This interface is provided for use with the extension point defined in this plugin. 
+ * It's a listener for als files opened in ALS editor.
+ */
 public interface IAlloyEditorListener {
 	/**
 	 * when an als file is loaded. 
 	 */
-	public void fileLoaded(IResource resource);
+	public void fileLoaded(IALSFile file);
 	/**
 	 * when an als file is saved. 
 	 */
-	public void fileSaved(IResource resource);
+	public void fileSaved(IALSFile file);
 	/**
 	 * when an als file change (not supported). 
 	 */
-	public void fileChanged(IResource resource);
+	public void fileChanged(IALSFile file);
 	/**
 	 * when an als file is closed. 
 	 */
-	public void fileClosed(IResource resource);
+	public void fileClosed(IALSFile file);
 	/**
 	 * when an als file has focus. 
 	 */
-	public void fileSetFocus(IResource resource);
+	public void fileSetFocus(IALSFile file);
 }
