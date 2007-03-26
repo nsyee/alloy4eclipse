3c29c45de91250618557822029f28d5b7b144991
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-26 10:08:15 +0000

    new launch configuration tab added (choose Command)

--- fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/ui/LaunchConfigurationTabGroups.java
+++ fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/ui/LaunchConfigurationTabGroups.java
@@ -1,9 +1,11 @@
 package fr.univartois.cril.alloyplugin.launch.ui;
 
+
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
 
 public class LaunchConfigurationTabGroups extends
 		AbstractLaunchConfigurationTabGroup {
@@ -15,9 +17,11 @@ public class LaunchConfigurationTabGroups extends
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		//TODO make a tab launch configuration (for choosing commands to be executed, solver etc...)				 
 		CommonTab c=new CommonTab();
-		ILaunchConfigurationTab[] tab = new ILaunchConfigurationTab[1];
-		tab[0]=c;
-		setTabs(tab);
+		
+		LaunchConfigurationTab tabPerso=new LaunchConfigurationTab();
+		ILaunchConfigurationTab[] tabs = {c,tabPerso};
+		//tab[0]=c;
+		setTabs(tabs);
 	}
 
 }
