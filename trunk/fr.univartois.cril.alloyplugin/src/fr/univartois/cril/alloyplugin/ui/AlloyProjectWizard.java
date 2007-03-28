4087017a193896f6b80e44994d4227b71522327b
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-03-28 19:18:04 +0000

    open alloy perspective when create an alloy project

--- trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/AlloyProjectWizard.java
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/ui/AlloyProjectWizard.java
@@ -1,73 +1,38 @@
 package fr.univartois.cril.alloyplugin.ui;
 
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWizard;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jface.operation.*;
 import java.lang.reflect.InvocationTargetException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.*;
+
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
 import fr.univartois.cril.alloyplugin.ProjectNature;
 
-
-
-
-public class AlloyProjectWizard extends BasicNewProjectResourceWizard {
-	/**
-	 * 
-	 */
-	private void createProject(String projectName, IProgressMonitor monitor) throws CoreException
-	{
-
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-
-		IProject project = root.getProject(projectName);
-		IProjectDescription description = ResourcesPlugin.getWorkspace().newProjectDescription(project.getName());
-		//TODO create a Alloy nature which works
-		description.setNatureIds(new String[] {ProjectNature.NATURE_ID});
-		ICommand command = description.newCommand();
-		//TODO create a Alloy builder	      
-		// command.setBuilderName("Alloy builder");
-		description.setBuildSpec(new ICommand[] { command });
-
-		project.create(description,monitor);
-
-		project.open(monitor);
-		project.setDescription(description, null);
-	}
-
+public class AlloyProjectWizard extends Wizard implements INewWizard,IExecutableExtension {
 	private AlloyProjectWizardPage page;
 	private ISelection selection;
-
-	/**
-	 * Constructor for SampleNewWizard.
-	 */
+	private IConfigurationElement configElement;
 	public AlloyProjectWizard() {
-		super();
-		setNeedsProgressMonitor(true);
+		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * Adding the page to the wizard.
-	 */
-
-	public void addPages() {
-		page = new AlloyProjectWizardPage(selection);
-		addPage(page);
-	}
-
-	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
-	 */
+	@Override
 	public boolean performFinish() {
-		//final String containerName = page.getContainerName();
+//		final String containerName = page.getContainerName();
 		final String fileName = page.getProjectName();
 		IRunnableWithProgress op = new IRunnableWithProgress() {
 			public void run(IProgressMonitor monitor) throws InvocationTargetException {
@@ -89,15 +54,45 @@ public class AlloyProjectWizard extends BasicNewProjectResourceWizard {
 			MessageDialog.openError(getShell(), "Error", realException.getMessage());
 			return false;
 		}
+
+		BasicNewProjectResourceWizard.updatePerspective(configElement);
+		
+		//selectAndReveal(fSecondPage.getJavaProject().getProject());
+
 		return true;
 	}
 
-	/**
-	 * The worker method. It will find the container, create the
-	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
-	 */
 
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
+
+	}
+
+
+	public void addPages() {
+		page = new AlloyProjectWizardPage(selection);
+		addPage(page);
+	}
+
+	private void createProject(String projectName, IProgressMonitor monitor) throws CoreException
+	{
+
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+
+		IProject project = root.getProject(projectName);
+		IProjectDescription description = ResourcesPlugin.getWorkspace().newProjectDescription(project.getName());
+		//TODO create a Alloy nature which works
+		description.setNatureIds(new String[] {ProjectNature.NATURE_ID});
+		ICommand command = description.newCommand();
+		//TODO create a Alloy builder	      
+		// command.setBuilderName("Alloy builder");
+		description.setBuildSpec(new ICommand[] { command });
+
+		project.create(monitor);//description,monitor);
+		project.open(monitor);
+		project.setDescription(description, null);
+		
+	}
 	private void doFinish(
 			//String containerName,
 			String fileName,
@@ -107,15 +102,9 @@ public class AlloyProjectWizard extends BasicNewProjectResourceWizard {
 		createProject(fileName,monitor);
 	}
 
-
-
-	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
-	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
-	 */
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.selection = selection;
+	public void setInitializationData(IConfigurationElement config, String propertyName, Object data) throws CoreException {
+		 configElement=config;		 
 	}
+
 
 }
