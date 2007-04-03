7973e4cf3109eddb713b8c2ba2b2f0d9ef54bf4e
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2007-04-03 19:23:15 +0000

    Added icons from Alloy on Eclipse plugin on Outline view.

--- trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/core/ui/IALSSignature.java
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/core/ui/IALSSignature.java
@@ -5,6 +5,6 @@ package fr.univartois.cril.alloyplugin.core.ui;
  * Interface for an Alloy signature.
  * Others plugins which wants to use Alloy signatures can use this interface..
  * */
-public interface IALSSignature {
+public interface IALSSignature extends IALSTreeDecorated {
 
 }
