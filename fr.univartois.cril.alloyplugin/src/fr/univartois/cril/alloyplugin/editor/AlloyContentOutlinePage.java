7428436fa45164ff09ff1403b8c7eeddcd30d3be
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-04-04 08:11:55 +0000

    Added goto line feature to outline view.

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloyContentOutlinePage.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloyContentOutlinePage.java
@@ -5,10 +5,17 @@ package fr.univartois.cril.alloyplugin.editor;
 
 import java.util.logging.Logger;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
 
+import fr.univartois.cril.alloyplugin.core.ui.IALSTreeDecorated;
 
 /**
  * 
@@ -17,9 +24,11 @@ import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
 public class AlloyContentOutlinePage extends ContentOutlinePage {
 
 	private static final Logger log = Logger.getLogger("alloy");
-	private final ALSEditor editor;	
+
+	private final ALSEditor editor;
+
 	private TreeViewer viewer;
-	
+
 	public AlloyContentOutlinePage(ALSEditor editor) {
 		this.editor = editor;
 	}
@@ -32,27 +41,46 @@ public class AlloyContentOutlinePage extends ContentOutlinePage {
 		viewer.setLabelProvider(new AlloyTreeLabelProvider());
 		log.info("Adding content provider");
 		viewer.setContentProvider(new AlloyTreeContentProvider());
-		//IEditorInput input = editor.getEditorInput();
-		
-		setViewerInput();		
+		// IEditorInput input = editor.getEditorInput();
+		this.addSelectionChangedListener(new MySelectionListener());
+		setViewerInput();
 		viewer.expandAll();
 		viewer.refresh();
 		log.info("Creation look OK");
 	}
 
 	/**
-	 * Set newInput to the viewer. The input is updated from editor.
-	 * It's used when input changes in editor. (when an opened file in editor is renamed for example)
-	 * */
+	 * Set newInput to the viewer. The input is updated from editor. It's used
+	 * when input changes in editor. (when an opened file in editor is renamed
+	 * for example)
+	 */
 	public void setViewerInput() {
-		if (editor!=null)
-			{			
-			if(viewer!=null)
+		if (editor != null) {
+			if (viewer != null)
 				viewer.setInput(editor);
-			}
-		
+		}
+
 	}
-	
-	
+
+	class MySelectionListener implements ISelectionChangedListener {
+
+		public void selectionChanged(SelectionChangedEvent event) {
+
+			Object selection = ((TreeSelection) event.getSelection())
+					.getFirstElement();
+			if (selection instanceof IALSTreeDecorated) {
+				IALSTreeDecorated elem = (IALSTreeDecorated) selection;
+				IDocumentProvider provider = editor.getDocumentProvider();
+				IDocument document = provider.getDocument(editor
+						.getEditorInput());
+				try {
+					int start = document.getLineOffset(elem.getLine() - 1);
+					editor.selectAndReveal(start, 0);
+				} catch (BadLocationException x) {
+					// ignore
+				}
+			}
+		}
+	}
 
 }
