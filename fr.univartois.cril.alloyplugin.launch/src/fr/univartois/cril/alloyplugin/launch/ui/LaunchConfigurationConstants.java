a659cb088183311e4c040ce6d106b8eeab2a9a87
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-04-04 10:26:46 +0000

    fusion operated:3nd try

--- fr.univartois.cril.alloyplugin.launch/src/fr/univartois/cril/alloyplugin/launch/ui/LaunchConfigurationConstants.java
+++
@@ -1,9 +0,0 @@
-package fr.univartois.cril.alloyplugin.launch.ui;
-
-public class LaunchConfigurationConstants {
-	/**
-	 * Attribute for a list of the commands to be executed.
-	 */
-	protected static final String ATTRIBUTE_COMMANDS_LIST = "commands_list";
-
-}
