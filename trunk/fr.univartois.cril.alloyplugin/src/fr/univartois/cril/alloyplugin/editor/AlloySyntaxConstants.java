0a080263d8cf6c5067b0fb4322260bc6ad11a60b
AntoineBourre <AntoineBourre@751a51b9-0729-0410-b983-630554dc95c4>
2007-09-19 21:09:56 +0000

    add some completions (ctrl + space) in Alloy editor.

---
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloySyntaxConstants.java
@@ -0,0 +1,25 @@
+package fr.univartois.cril.alloyplugin.editor;
+
+/**
+ * Alloy syntax constants. 
+ */
+public class AlloySyntaxConstants {
+	
+	
+	public static final String[] keywords = { "abstract", "and", "assert",
+		"but", "disj", "else", "exactly", "extends", "fact", "fun", "iden",
+		"if", "iff", "implies", "in", "Int", "int", "let", "not", "or",
+		"part", "pred", "set", "seq", "sig", "sum", "then", "this", "univ" // doute
+																			// sur
+																			// this
+};
+
+public static final String[] quantifiers = { "all", "lone", "no", "none",
+		"one", "some" };
+
+/** commands */
+public static final String[] commands = { "run", "check", "for", "expect" };
+
+/** directives */
+public static final String[] directives = { "module", "open", "as" };
+}
