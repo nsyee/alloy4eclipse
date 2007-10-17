41744fb8c697900520c5fdd63839f57a1c8f12b1
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2007-10-17 20:51:10 +0000

    Fixed name convention violation (Leading uppercase letter in a method name).

--- trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/core/AlloyLaunching.java
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/core/AlloyLaunching.java
@@ -34,225 +34,236 @@ import fr.univartois.cril.alloyplugin.core.ui.IALSSignature;
 import fr.univartois.cril.alloyplugin.launch.util.Util;
 import fr.univartois.cril.alloyplugin.preferences.PreferenceConstants;
 
-
 /**
  * Static methods to launch Alloy parser or execute a command.
- * 
- * */
+ */
 public class AlloyLaunching {
 
-	/** 
-	 * Execute an ExecutableCommand previously created after a parsing.
-	 */
-	public static final void ExecCommand(ExecutableCommand command){
-		assert(command!=null);
-		Reporter rep=new Reporter(command.getResource());
-		execCommand(command,rep);
-	}	
+    /**
+     * Execute an ExecutableCommand previously created after a parsing.
+     */
+    public static final void execCommand(ExecutableCommand command) {
+        assert (command != null);
+        Reporter rep = new Reporter(command.getResource());
+        execCommand(command, rep);
+    }
 
-	/**
-	 * Parse a als file.
-	 * @return an array (can be empty if there is no command in the file.)
-	 */
-	public void launchParser(ALSFile file) {				
-		if (file==null||!file.getResource().exists()) return;
-		IResource res = file.getResource();
-		try {
-			res.deleteMarkers(fr.univartois.cril.alloyplugin.ui.Util.ALLOYPROBLEM, false,0);
-		} catch (CoreException e) {
-			e.printStackTrace();
-		} 
-		Reporter rep=new Reporter(res);
+    /**
+     * Parse a als file.
+     * 
+     * @return an array (can be empty if there is no command in the file.)
+     */
+    public void launchParser(ALSFile file) {
+        if (file == null || !file.getResource().exists())
+            return;
+        IResource res = file.getResource();
+        try {
+            res.deleteMarkers(
+                    fr.univartois.cril.alloyplugin.ui.Util.ALLOYPROBLEM, false,
+                    0);
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+        Reporter rep = new Reporter(res);
 
-		try {
-			AlloyLaunching.parse(file,rep);
-		} catch (Err e) {			
-			displayErrorInProblemView(res, e);					
-		}				
-	}
+        try {
+            AlloyLaunching.parse(file, rep);
+        } catch (Err e) {
+            displayErrorInProblemView(res, e);
+        }
+    }
 
-	/**
-	 * Parse one file.(not its subfiles)
-	 * Don't update associated ALSFile. 
-	 * */
-	public List<Command> parseOneFile(IResource resource) {
-		if (!resource.exists()) return null;
-		try {
-			resource.deleteMarkers(fr.univartois.cril.alloyplugin.ui.Util.ALLOYPROBLEM, false,0);
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}  
-		List<Command> list=null;
-		try {
-			list = CompUtil.parseOneModule_fromFile(resource.getLocation().toString());
-		} catch (Err e) {			
-			displayErrorInProblemView(resource, e);					
-		}				
-		return list;
+    /**
+     * Parse one file.(not its subfiles) Don't update associated ALSFile.
+     */
+    public List<Command> parseOneFile(IResource resource) {
+        if (!resource.exists())
+            return null;
+        try {
+            resource.deleteMarkers(
+                    fr.univartois.cril.alloyplugin.ui.Util.ALLOYPROBLEM, false,
+                    0);
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+        List<Command> list = null;
+        try {
+            list = CompUtil.parseOneModule_fromFile(resource.getLocation()
+                    .toString());
+        } catch (Err e) {
+            displayErrorInProblemView(resource, e);
+        }
+        return list;
 
-	}
-	/**
-	 * Displays an Err exception in problem view, except if it starts with "Solver fatal exception".
-	 */
-	public static void displayErrInProblemView(IResource res, Err e, int severity) {
-		if(e.msg.startsWith("Solver fatal exception"))
-		{
-			DisplaySolverFatalError();			
-		}
-		else
-		{
+    }
 
-			res= getResourceFromErr(res, e);
-			try {
-				IMarker marker = res.createMarker(fr.univartois.cril.alloyplugin.ui.Util.ALLOYPROBLEM);
-				marker.setAttribute(IMarker.SEVERITY,severity);
-				marker.setAttribute(IMarker.LINE_NUMBER, e.pos.y);
-				marker.setAttribute(IMarker.MESSAGE, e.msg);
-			} catch (CoreException e1) {
-				e1.printStackTrace();
-			}
-		}
-	}	
-	/**
-	 * Display a solver fatal error in a SWT thread.
-	 */
-	private static void DisplaySolverFatalError() {
-		Display display = PlatformUI.getWorkbench().getDisplay();        
-		if (display!=null)
-			display.asyncExec(
-					new Runnable() {
-						public void run(){								
-							String solverName=AlloyPlugin.getDefault().getPreferenceStore().getString(PreferenceConstants.P_SOLVER_CHOICE);
-							Shell shell = new Shell();
-							MessageDialog.openInformation(
-									shell,
-									"Alloy Plug-in",
-									"\nSolver fatal exception: "+solverName+" maybe not found.");								
-						}
-					});		
-	}
+    /**
+     * Displays an Err exception in problem view, except if it starts with
+     * "Solver fatal exception".
+     */
+    public static void displayErrInProblemView(IResource res, Err e,
+            int severity) {
+        if (e.msg.startsWith("Solver fatal exception")) {
+            displaySolverFatalError();
+        } else {
 
-	public static void displayErrorInProblemView(IResource res, Err e) {
-		displayErrInProblemView(res,e,IMarker.SEVERITY_ERROR);
-	}    
-	public static void displayWarningInProblemView(IResource res, Err e) {
-		displayErrInProblemView(res,e,IMarker.SEVERITY_WARNING);
-	}	
-	/**
-	 * Get the ressource where the Err is located. 
-	 */
-	private  static IResource getResourceFromErr(IResource res,Err e)
-	{
-		if(e.pos!=Pos.UNKNOWN)
-		{
-			if(!e.pos.filename.equals(Util.getFileLocation(res)))
-			{
-				return Util.getFileForLocation(e.pos.filename);
-			}
-		}
-		return res;
-	}
+            res = getResourceFromErr(res, e);
+            try {
+                IMarker marker = res
+                        .createMarker(fr.univartois.cril.alloyplugin.ui.Util.ALLOYPROBLEM);
+                marker.setAttribute(IMarker.SEVERITY, severity);
+                marker.setAttribute(IMarker.LINE_NUMBER, e.pos.y);
+                marker.setAttribute(IMarker.MESSAGE, e.msg);
+            } catch (CoreException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
 
+    /**
+     * Display a solver fatal error in a SWT thread.
+     */
+    private static void displaySolverFatalError() {
+        Display display = PlatformUI.getWorkbench().getDisplay();
+        if (display != null)
+            display.asyncExec(new Runnable() {
+                public void run() {
+                    String solverName = AlloyPlugin.getDefault()
+                            .getPreferenceStore().getString(
+                                    PreferenceConstants.P_SOLVER_CHOICE);
+                    Shell shell = new Shell();
+                    MessageDialog.openInformation(shell, "Alloy Plug-in",
+                            "\nSolver fatal exception: " + solverName
+                                    + " maybe not found.");
+                }
+            });
+    }
 
+    public static void displayErrorInProblemView(IResource res, Err e) {
+        displayErrInProblemView(res, e, IMarker.SEVERITY_ERROR);
+    }
 
-	/**
-	 * Parse a .als file. The ALSfile fields are modidied.
-	 * @throws Err 
-	 * */
-	private static void parse(ALSFile file,Reporter rep) throws Err 
-	{IResource res=file.getResource();
-	String filename = res.getLocation().toString();
-	AlloyMessageConsole alloyParserConsole=Console.findAlloyInfoConsole(filename);
-	alloyParserConsole.clear();
-	alloyParserConsole.printInfo("=========== Parsing \""+filename+"\" =============");
-	Module world;
-	world = CompUtil.parseEverything_fromFile(rep, null, filename);	
-	alloyParserConsole.printInfo("=========== End Parsing \""+filename+"\" =============");
-	updateALSFile(world,file);	
-	}
+    public static void displayWarningInProblemView(IResource res, Err e) {
+        displayErrInProblemView(res, e, IMarker.SEVERITY_WARNING);
+    }
 
+    /**
+     * Get the ressource where the Err is located.
+     */
+    private static IResource getResourceFromErr(IResource res, Err e) {
+        if (e.pos != Pos.UNKNOWN) {
+            if (!e.pos.filename.equals(Util.getFileLocation(res))) {
+                return Util.getFileForLocation(e.pos.filename);
+            }
+        }
+        return res;
+    }
 
-	/**
-	 * Set the fields of an alsFile. (commands, signatures..)
-	 * fire changed() on the als file for listeners.
-	 * */
-	private static void updateALSFile(Module world, ALSFile file) throws Err {
-//		convert all commands in ExecutableCommand[]
-		List<Pair<Command,Expr>> list = world.getAllCommandsWithFormulas();
-		List<IALSCommand>  exec_cmds=new ArrayList<IALSCommand>();//new ExecutableCommand[list.size()];		
+    /**
+     * Parse a .als file. The ALSfile fields are modidied.
+     * 
+     * @throws Err
+     */
+    private static void parse(ALSFile file, Reporter rep) throws Err {
+        IResource res = file.getResource();
+        String filename = res.getLocation().toString();
+        AlloyMessageConsole alloyParserConsole = Console
+                .findAlloyInfoConsole(filename);
+        alloyParserConsole.clear();
+        alloyParserConsole.printInfo("=========== Parsing \"" + filename
+                + "\" =============");
+        Module world;
+        world = CompUtil.parseEverything_fromFile(rep, null, filename);
+        alloyParserConsole.printInfo("=========== End Parsing \"" + filename
+                + "\" =============");
+        updateALSFile(world, file);
+    }
 
-		for (Pair<Command,Expr> command : list) {			
-			exec_cmds.add(new ExecutableCommand(file, command.a, command.b, world));
-		}				
-		file.setCommand(exec_cmds);
-		SafeList<Pair<String,Expr>> factsList=world.getAllFacts();
-		List<IALSFact> facts=new ArrayList<IALSFact>(factsList.size());		
-		for(Pair<String,Expr> fact : factsList){
-			facts.add(new Fact(fact));
-		}		
-		file.setFacts(facts);
+    /**
+     * Set the fields of an alsFile. (commands, signatures..) fire changed() on
+     * the als file for listeners.
+     */
+    private static void updateALSFile(Module world, ALSFile file) throws Err {
+        // convert all commands in ExecutableCommand[]
+        List<Pair<Command, Expr>> list = world.getAllCommandsWithFormulas();
+        List<IALSCommand> exec_cmds = new ArrayList<IALSCommand>();// new
+                                                                    // ExecutableCommand[list.size()];
 
-		SafeList<Func> funcList=world.getAllFunc();
-		List<IALSFunction> funcs=new ArrayList<IALSFunction>(funcList.size());	
-		List<IALSPredicate> preds=new ArrayList<IALSPredicate>(funcList.size());  
-		for(Func fun : funcList){
-			if (fun.isPred) {
-				preds.add(new Predicate(fun));
-			} else {
-				funcs.add(new Function(fun));
-			}
-		}
-		file.setFunctions(funcs);
-		file.setPredicates(preds);
-		SafeList<Sig> sigList=world.getAllSigs();
-		List<IALSSignature> sigs=new ArrayList<IALSSignature>(sigList.size());		
-		for(Sig sig : sigList){
-			sigs.add(new Signature(sig));
-		}
-		file.setSignatures(sigs);
-		file.fireChange();
-		System.out.println("ALSFile changed:"+file);
-	}
+        for (Pair<Command, Expr> command : list) {
+            exec_cmds.add(new ExecutableCommand(file, command.a, command.b,
+                    world));
+        }
+        file.setCommand(exec_cmds);
+        SafeList<Pair<String, Expr>> factsList = world.getAllFacts();
+        List<IALSFact> facts = new ArrayList<IALSFact>(factsList.size());
+        for (Pair<String, Expr> fact : factsList) {
+            facts.add(new Fact(fact));
+        }
+        file.setFacts(facts);
 
-	/**
-	 * Execute a command. The command is modified. Some informations can be show to console. 
-	 */
-	private static final void execCommand(ExecutableCommand command,Reporter rep)  {
-		AlloyMessageConsole alloyConsole=Console.findAlloyConsole(command.getFilename());
-		alloyConsole.activate();
-		try {
+        SafeList<Func> funcList = world.getAllFunc();
+        List<IALSFunction> funcs = new ArrayList<IALSFunction>(funcList.size());
+        List<IALSPredicate> preds = new ArrayList<IALSPredicate>(funcList
+                .size());
+        for (Func fun : funcList) {
+            if (fun.isPred) {
+                preds.add(new Predicate(fun));
+            } else {
+                funcs.add(new Function(fun));
+            }
+        }
+        file.setFunctions(funcs);
+        file.setPredicates(preds);
+        SafeList<Sig> sigList = world.getAllSigs();
+        List<IALSSignature> sigs = new ArrayList<IALSSignature>(sigList.size());
+        for (Sig sig : sigList) {
+            sigs.add(new Signature(sig));
+        }
+        file.setSignatures(sigs);
+        file.fireChange();
+        System.out.println("ALSFile changed:" + file);
+    }
 
-			alloyConsole.printInfo("============ Command "+command+": ============");
-			A4Solution ans;
+    /**
+     * Execute a command. The command is modified. Some informations can be show
+     * to console.
+     */
+    private static final void execCommand(ExecutableCommand command,
+            Reporter rep) {
+        AlloyMessageConsole alloyConsole = Console.findAlloyConsole(command
+                .getFilename());
+        alloyConsole.activate();
+        try {
 
-			ans=command.execute(rep);
-			// Print the outcome in console
-			alloyConsole.printInfo("============ Answer ============");
-			alloyConsole.print(ans.toString());
+            alloyConsole.printInfo("============ Command " + command
+                    + ": ============");
+            A4Solution ans;
 
+            ans = command.execute(rep);
+            // Print the outcome in console
+            alloyConsole.printInfo("============ Answer ============");
+            alloyConsole.print(ans.toString());
 
-		} catch (Err e) {			
-			displayErrorInProblemView(command.getResource(), e);
-		}
+        } catch (Err e) {
+            displayErrorInProblemView(command.getResource(), e);
+        }
 
+    }
 
-	}
-
-	/**
-	 * Old method for saving the answer in a temporary file and visualize it. 
-	 * @param ans
-	 * @throws Err
-	 */
-	@Deprecated
-	public static void displayAns(A4Solution ans) throws Err {
-//		GraphView.Visualize(ans);		
-		ans.writeXML("output.xml");
-		//
-		// You can then visualize the XML file by calling this:
-		VizGUI viz = new VizGUI(false,"",null);
-		viz.run(VizGUI.evs_loadInstanceForcefully, "output.xml");	
-	}
+    /**
+     * Old method for saving the answer in a temporary file and visualize it.
+     * 
+     * @param ans
+     * @throws Err
+     */
+    @Deprecated
+    public static void displayAns(A4Solution ans) throws Err {
+        // GraphView.Visualize(ans);
+        ans.writeXML("output.xml");
+        //
+        // You can then visualize the XML file by calling this:
+        VizGUI viz = new VizGUI(false, "", null);
+        viz.run(VizGUI.evs_loadInstanceForcefully, "output.xml");
+    }
 
 }
-
-
