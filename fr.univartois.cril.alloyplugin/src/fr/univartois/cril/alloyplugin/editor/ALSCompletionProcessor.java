d1798d64b6c5fdf4f5501b6febbf28bf00735435
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2007-11-05 21:48:08 +0000

    Fixed content assist for Alloy language keywords (simple word completion, does not take into account the context).

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/ALSCompletionProcessor.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/ALSCompletionProcessor.java
@@ -1,6 +1,11 @@
 
 package fr.univartois.cril.alloyplugin.editor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
@@ -16,13 +21,20 @@ public class ALSCompletionProcessor implements IContentAssistProcessor {
 
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer,
 			int offset) {
-		CompletionProposal[] tab=new CompletionProposal[AlloySyntaxConstants.keywords.length];
-		for(int i=0;i<AlloySyntaxConstants.keywords.length;i++)
-		{
-			tab[i]=new CompletionProposal(AlloySyntaxConstants.keywords[i],offset,0,0);	
-		}
-		
-		return tab;
+	    
+	    List<CompletionProposal> props = new ArrayList<CompletionProposal>();
+        try {
+            String prefix = getPrefix(viewer,offset);
+            for(String keyword : AlloySyntaxConstants.keywords)
+            {
+            	if (keyword.startsWith(prefix))
+            	    props.add(new CompletionProposal(keyword,offset-prefix.length(),prefix.length(),keyword.length()));	
+            }
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+		CompletionProposal [] tab = new CompletionProposal[props.size()];
+		return props.toArray(tab);
 	}
 
 	
@@ -51,5 +63,24 @@ public class ALSCompletionProcessor implements IContentAssistProcessor {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	 /**
+     * @see http://dev.eclipse.org/newslists/news.eclipse.modeling.gmf/msg01324.html
+     * @param viewer
+     * @param offset
+     * @return the text input by the user
+     * @throws BadLocationException
+     */
+    private String getPrefix(ITextViewer viewer, int offset) throws BadLocationException {
+        IDocument doc= viewer.getDocument();
+        if (doc == null || offset > doc.getLength())
+            return null;
+        
+        int length= 0;
+        while (--offset >= 0 && Character.isJavaIdentifierPart(doc.getChar(offset)))
+            length++;
+        
+        return doc.get(offset + 1, length);
+    }
 
 }
