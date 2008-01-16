a137b12dbdb527e3fef0967baa6a6ebea6847abb
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2008-01-16 19:37:35 +0000

    Added private in the list of keywords.

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloySyntaxConstants.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloySyntaxConstants.java
@@ -1,25 +1,23 @@
 package fr.univartois.cril.alloyplugin.editor;
 
 /**
- * Alloy syntax constants. 
+ * Alloy syntax constants.
  */
 public class AlloySyntaxConstants {
-	
-	
-	public static final String[] keywords = { "abstract", "and", "assert",
-		"but", "disj", "else", "exactly", "extends", "fact", "fun", "iden",
-		"if", "iff", "implies", "in", "Int", "int", "let", "not", "or",
-		"part", "pred", "set", "seq", "sig", "sum", "then", "this", "univ" // doute
-																			// sur
-																			// this
-};
 
-public static final String[] quantifiers = { "all", "lone", "no", "none",
-		"one", "some" };
+    public static final String[] keywords    = { "abstract", "and", "assert",
+            "but", "disj", "else", "exactly", "extends", "fact", "fun", "iden",
+            "if", "iff", "implies", "in", "Int", "int", "let", "not", "or",
+            "part", "pred", "set", "seq", "sig", "sum", "then", "this", "univ",
+            "private"                       };
 
-/** commands */
-public static final String[] commands = { "run", "check", "for", "expect" };
+    public static final String[] quantifiers = { "all", "lone", "no", "none",
+            "one", "some"                   };
 
-/** directives */
-public static final String[] directives = { "module", "open", "as" };
+    /** commands */
+    public static final String[] commands    = { "run", "check", "for",
+            "expect"                        };
+
+    /** directives */
+    public static final String[] directives  = { "module", "open", "as" };
 }
