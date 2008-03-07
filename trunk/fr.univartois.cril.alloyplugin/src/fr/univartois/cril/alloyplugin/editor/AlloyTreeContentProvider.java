2059722beb5185e838507701fee31cca8e7393c8
rdruelle <rdruelle@751a51b9-0729-0410-b983-630554dc95c4>
2008-03-07 15:38:34 +0000

    The feature based on unsat core is finished. When the solver is answering unsat, it is possible to highlight the part of the specification that is responsible for the inconsistency. It works.

--- trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloyTreeContentProvider.java
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloyTreeContentProvider.java
@@ -22,198 +22,188 @@ import fr.univartois.cril.alloyplugin.api.IALSPredicate;
 import fr.univartois.cril.alloyplugin.api.IALSSignature;
 import fr.univartois.cril.alloyplugin.api.IALSTreeDecorated;
 import fr.univartois.cril.alloyplugin.api.IAlloyTreeContentProvider;
+import fr.univartois.cril.alloyplugin.api.NodeWithChildren;
+import fr.univartois.cril.alloyplugin.core.ExecutableCommand;
 
 public class AlloyTreeContentProvider implements IAlloyTreeContentProvider,
-        IALSFileListener {
+		IALSFileListener {
 
-    public final RootContent<IALSFunction>      FUNCTIONS  = new RootContent<IALSFunction>(
-                                                     "Functions",
-                                                     ALSImageRegistry
-                                                             .getImage(
-                                                                     ALSImageRegistry.FUNCTION_ICON_ID));
-    public final RootContent<IALSSignature>      SIGNATURES = new RootContent<IALSSignature>(
-                                                     "Signatures",
-                                                     ALSImageRegistry
-                                                             .getImage(
-                                                                     ALSImageRegistry.SIGNATURE_ICON_ID));
-    public final RootContent<IALSFact>      FACTS      = new RootContent<IALSFact>(
-                                                     "Facts",
-                                                     ALSImageRegistry
-                                                             .getImage(
-                                                                     ALSImageRegistry.FACT_ICON_ID));
-    public final RootContent<IALSAssert>      ASSERT     = new RootContent<IALSAssert>(
-                                                     "Assertions",
-                                                     ALSImageRegistry
-                                                             .getImage(
-                                                                     ALSImageRegistry.ASSERT_ICON_ID));
-    public final RootContent<IALSPredicate>      PREDICATES = new RootContent<IALSPredicate>(
-                                                     "Predicates",
-                                                     ALSImageRegistry
-                                                             .getImage(
-                                                                     ALSImageRegistry.PREDICATE_ICON_ID));
-    public final RootContent<IALSCommand>      COMMANDS   = new RootContent<IALSCommand>(
-                                                     "Commands",
-                                                     ALSImageRegistry
-                                                             .getImage(
-                                                                     ALSImageRegistry.COMMAND_ID));
+	public final RootContent<IALSFunction> FUNCTIONS = new RootContent<IALSFunction>(
+			"Functions", ALSImageRegistry
+					.getImage(ALSImageRegistry.FUNCTION_ICON_ID));
+	public final RootContent<IALSSignature> SIGNATURES = new RootContent<IALSSignature>(
+			"Signatures", ALSImageRegistry
+					.getImage(ALSImageRegistry.SIGNATURE_ICON_ID));
+	public final RootContent<IALSFact> FACTS = new RootContent<IALSFact>(
+			"Facts", ALSImageRegistry.getImage(ALSImageRegistry.FACT_ICON_ID));
+	public final RootContent<IALSAssert> ASSERT = new RootContent<IALSAssert>(
+			"Assertions", ALSImageRegistry
+					.getImage(ALSImageRegistry.ASSERT_ICON_ID));
+	public final RootContent<IALSPredicate> PREDICATES = new RootContent<IALSPredicate>(
+			"Predicates", ALSImageRegistry
+					.getImage(ALSImageRegistry.PREDICATE_ICON_ID));
+	public final RootContent<IALSCommand> COMMANDS = new RootContent<IALSCommand>(
+			"Commands", ALSImageRegistry.getImage(ALSImageRegistry.COMMAND_ID));
 
-    private ALSEditor             editor;
-    private IALSFile              af;
-    private Viewer                viewer;
-    // private static final Logger log = Logger.getLogger("alloy");
-    private static final Object[] EMPTY_TAB  = new Object[0];
+	private ALSEditor editor;
+	private IALSFile af;
+	private Viewer viewer;
+	// private static final Logger log = Logger.getLogger("alloy");
+	private static final Object[] EMPTY_TAB = new Object[0];
 
-    public AlloyTreeContentProvider() {
-        super();
+	public AlloyTreeContentProvider() {
+		super();
 
-    }
+	}
 
-    public void dispose() {
-        // log.info("Dispose");
+	public void dispose() {
+		// log.info("Dispose");
 
-        // removeListeningALSFile();
+		// removeListeningALSFile();
 
-    }
+	}
 
-    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-        // System.out.println("input changed: new Input" + newInput);
-        this.viewer = viewer;
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// System.out.println("input changed: new Input" + newInput);
+		this.viewer = viewer;
 
-        if (oldInput != null) {
-            removeListeningALSFile();
-        }
-        editor = (ALSEditor) newInput;
-        if (newInput != null) {
-            addListeningALSFile();
-            feedAnnotations();
-            editor.updateFoldingStructure(positions);
-        }
+		if (oldInput != null) {
+			removeListeningALSFile();
+		}
+		editor = (ALSEditor) newInput;
+		if (newInput != null) {
+			addListeningALSFile();
+			feedAnnotations();
+			editor.updateFoldingStructure(positions);
+		}
 
-        // IDocument document= fDocumentProvider.getDocument(newInput);
-        // if (document != null) {
-        // document.addPositionCategory(SEGMENTS);
-        // document.addPositionUpdater(fPositionUpdater);
-        // parse(document);
-        // }
-        // }
-    }
+		// IDocument document= fDocumentProvider.getDocument(newInput);
+		// if (document != null) {
+		// document.addPositionCategory(SEGMENTS);
+		// document.addPositionUpdater(fPositionUpdater);
+		// parse(document);
+		// }
+		// }
+	}
 
-    /**
-     * Make this class listens changes on ALSFile from editor.
-     */
-    private void addListeningALSFile() {
-        af = editor.getALSFile();
+	/**
+	 * Make this class listens changes on ALSFile from editor.
+	 */
+	private void addListeningALSFile() {
+		af = editor.getALSFile();
 
-        if (af != null) {
-            af.addListener(this);
-            updateTreeContentFromA4AST();
-        }
-    }
+		if (af != null) {
+			af.addListener(this);
+			updateTreeContentFromA4AST();
+		}
+	}
 
-    private void updateTreeContentFromA4AST() {
-        SIGNATURES.setChildren(af.getSignatures());
-        FACTS.setChildren(af.getFacts());
-        FUNCTIONS.setChildren(af.getFunctions());
-        ASSERT.setChildren(af.getAssertions());
-        PREDICATES.setChildren(af.getPredicates());
-        COMMANDS.setChildren(af.getCommand());
-    }
+	private void updateTreeContentFromA4AST() {
+		SIGNATURES.setChildren(af.getSignatures());
+		FACTS.setChildren(af.getFacts());
+		FUNCTIONS.setChildren(af.getFunctions());
+		ASSERT.setChildren(af.getAssertions());
+		PREDICATES.setChildren(af.getPredicates());
+		COMMANDS.setChildren(af.getCommand());
+	}
 
-    /**
-     * Make this class doesn't listen the ALSFile anymore.
-     */
-    private void removeListeningALSFile() {
-        if (af != null) {
-            af.removeListener(this);
-            af = null;
-        }
-    }
+	/**
+	 * Make this class doesn't listen the ALSFile anymore.
+	 */
+	private void removeListeningALSFile() {
+		if (af != null) {
+			af.removeListener(this);
+			af = null;
+		}
+	}
 
-    public Object[] getChildren(Object parentElement) {
-        // log.info("Get children for "+parentElement);
-        if (af == null)
-            return EMPTY_TAB;
+	public Object[] getChildren(Object parentElement) {
+		// log.info("Get children for "+parentElement);
+		if (af == null)
+			return EMPTY_TAB;
 
-        if (parentElement instanceof RootContent) {
-            return ((RootContent) parentElement).children();
-        }
-        return EMPTY_TAB;
-    }
+		if (parentElement instanceof NodeWithChildren) {
+			return ((NodeWithChildren) parentElement).children();
+		}
 
-    public Object getParent(Object element) {
-        // log.info("Get parent for "+element);
-        return null;
-    }
+		return EMPTY_TAB;
+	}
 
-    public boolean hasChildren(Object element) {
-        if (element instanceof RootContent) {
-            return ((RootContent) element).hasChildren();
-        }
-        return false;
-    }
+	public Object getParent(Object element) {
+		// log.info("Get parent for "+element);
+		return null;
+	}
 
-    public Object[] getElements(Object inputElement) {
-        // log.info("get elements for "+inputElement);
-        // System.out.println("get elements for :" + inputElement);
-        return new RootContent[] { SIGNATURES, FACTS, ASSERT, FUNCTIONS,
-                PREDICATES, COMMANDS };
-    }
+	public boolean hasChildren(Object element) {
+		if (element instanceof NodeWithChildren) {
+			return ((NodeWithChildren) element).hasChildren();
+		}
+		return false;
+	}
 
-    private ArrayList<Position> positions = new ArrayList<Position>();
+	public Object[] getElements(Object inputElement) {
+		// log.info("get elements for "+inputElement);
+		// System.out.println("get elements for :" + inputElement);
+		return new RootContent[] { SIGNATURES, FACTS, ASSERT, FUNCTIONS,
+				PREDICATES, COMMANDS };
+	}
 
-    private void feedAnnotations() {
-        if (af==null) return;
-        positions.clear();
-        IDocument document = editor.getDocumentProvider().getDocument(
-                editor.getEditorInput());
-        List<IALSTreeDecorated> alloyobjects = af.getAllAlloyObjects();
-        int nol = document.getNumberOfLines();
-        for (IALSTreeDecorated ao : alloyobjects) {
-            try {
-                int bol = ao.getBeginLine();
-                int eol = ao.getEndLine();
-                // System.out.println(ao.toString()+":"+bol+"<"+eol);
-                if (bol>0 && eol - bol > 1) {
-                    int offset = document.getLineOffset(bol-1);
-                    int endOffset = (eol == nol) ? document.getLength()
-                            : document.getLineOffset(eol);
-                    assert offset < endOffset : ao.toString() + ":" + offset
-                            + "<" + endOffset;
-                    // System.out.println(ao.toString()+":"+offset+"<"+endOffset);
-                    positions.add(new Position(offset, endOffset - offset));
-                }
-            } catch (BadLocationException e) {
-                e.printStackTrace();
-            }
-        }
+	private ArrayList<Position> positions = new ArrayList<Position>();
 
-    }
+	private void feedAnnotations() {
+		if (af == null)
+			return;
+		positions.clear();
+		IDocument document = editor.getDocumentProvider().getDocument(
+				editor.getEditorInput());
+		List<IALSTreeDecorated> alloyobjects = af.getAllAlloyObjects();
+		int nol = document.getNumberOfLines();
+		for (IALSTreeDecorated ao : alloyobjects) {
+			try {
+				int bol = ao.getBeginLine();
+				int eol = ao.getEndLine();
+				// System.out.println(ao.toString()+":"+bol+"<"+eol);
+				if (bol > 0 && eol - bol > 1) {
+					int offset = document.getLineOffset(bol - 1);
+					int endOffset = (eol == nol) ? document.getLength()
+							: document.getLineOffset(eol);
+					assert offset < endOffset : ao.toString() + ":" + offset
+							+ "<" + endOffset;
+					// System.out.println(ao.toString()+":"+offset+"<"+endOffset);
+					positions.add(new Position(offset, endOffset - offset));
+				}
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+		}
 
-    /**
-     * This method is called when changes are made on the ALSFile. The viewer is
-     * refresh for displaying changes. It's done in a SWT thread.
-     */
-    public void changed(IALSFile file) {
-        feedAnnotations();
-        Display display = PlatformUI.getWorkbench().getDisplay();
-        if (display != null)
-            display.asyncExec(new Runnable() {
-                public void run() {
-                    updateTreeContentFromA4AST();
-                    ((TreeViewer) viewer).expandAll();
-                    viewer.refresh();
-                    editor.updateFoldingStructure(positions);
-                }
-            });
-    }
+	}
 
-    public IALSCommand[] getAllCommands() {
-         IALSCommand [] commands = new IALSCommand[COMMANDS.size()];
-         return COMMANDS.children(commands);
-    }
+	/**
+	 * This method is called when changes are made on the ALSFile. The viewer is
+	 * refresh for displaying changes. It's done in a SWT thread.
+	 */
+	public void changed(IALSFile file) {
+		feedAnnotations();
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		if (display != null)
+			display.asyncExec(new Runnable() {
+				public void run() {
+					updateTreeContentFromA4AST();
+					((TreeViewer) viewer).expandAll();
+					viewer.refresh();
+					editor.updateFoldingStructure(positions);
+				}
+			});
+	}
 
-    public Object getCommandsRootContent() {
-        return COMMANDS;
-    }
+	public IALSCommand[] getAllCommands() {
+		IALSCommand[] commands = new IALSCommand[COMMANDS.size()];
+		return COMMANDS.children(commands);
+	}
+
+	public Object getCommandsRootContent() {
+		return COMMANDS;
+	}
 
 }
