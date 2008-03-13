9ae1f4d9cc17567960b274211b54d24902572934
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2008-03-13 20:02:58 +0000

    Fixed various bugs related to the unsat core feature.

--- fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloyContentOutlinePage.java
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/editor/AlloyContentOutlinePage.java
@@ -40,216 +40,218 @@ import fr.univartois.cril.alloyplugin.preferences.PreferenceConstants;
  */
 public class AlloyContentOutlinePage extends ContentOutlinePage {
 
-	// private static final Logger log = Logger.getLogger("alloy");
+    // private static final Logger log = Logger.getLogger("alloy");
 
-	private final ALSEditor editor;
+    private final ALSEditor            editor;
 
-	private LaunchCommandAction launchCommandAction;
-	private DisplayCommandAnswerAction displayCommandAnswerAction;
+    private LaunchCommandAction        launchCommandAction;
+    private DisplayCommandAnswerAction displayCommandAnswerAction;
 
-	private TreeViewer viewer;
+    private TreeViewer                 viewer;
 
-	public AlloyContentOutlinePage(ALSEditor editor) {
-		this.editor = editor;
-	}
+    public AlloyContentOutlinePage(ALSEditor editor) {
+        this.editor = editor;
+    }
 
-	public void createControl(Composite parent) {
-		super.createControl(parent);
-		// log.info("Creating Outline Page");
-		viewer = getTreeViewer();
-		// log.info("Adding label provider");
-		viewer.setLabelProvider(new AlloyTreeLabelProvider());
-		// log.info("Adding content provider");
-		viewer.setContentProvider(new AlloyTreeContentProvider());
-		// IEditorInput input = editor.getEditorInput();
-		this.addSelectionChangedListener(new MySelectionListener());
-		setViewerInput();
-		viewer.expandAll();
-		viewer.refresh();
+    public void createControl(Composite parent) {
+        super.createControl(parent);
+        // log.info("Creating Outline Page");
+        viewer = getTreeViewer();
+        // log.info("Adding label provider");
+        viewer.setLabelProvider(new AlloyTreeLabelProvider());
+        // log.info("Adding content provider");
+        viewer.setContentProvider(new AlloyTreeContentProvider());
+        // IEditorInput input = editor.getEditorInput();
+        this.addSelectionChangedListener(new MySelectionListener());
+        setViewerInput();
+        viewer.expandAll();
+        viewer.refresh();
 
-		// pour le menu
+        // pour le menu
 
-		makeActions();
-		hookContextMenu();
-		hookDoubleClickAction();
-		contributeToActionBars();
+        makeActions();
+        hookContextMenu();
+        hookDoubleClickAction();
+        contributeToActionBars();
 
-		// log.info("Creation look OK");
-	}
+        // log.info("Creation look OK");
+    }
 
-	/**
-	 * Set newInput to the viewer. The input is updated from editor. It's used
-	 * when input changes in editor. (when an opened file in editor is renamed
-	 * for example)
-	 */
-	public void setViewerInput() {
-		if (editor != null) {
-			if (viewer != null)
-				viewer.setInput(editor);
-		}
+    /**
+     * Set newInput to the viewer. The input is updated from editor. It's used
+     * when input changes in editor. (when an opened file in editor is renamed
+     * for example)
+     */
+    public void setViewerInput() {
+        if (editor != null) {
+            if (viewer != null)
+                viewer.setInput(editor);
+        }
 
-	}
+    }
 
-	private void hookContextMenu() {
-		MenuManager menuMgr = new MenuManager("#PopupMenu");
-		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener() {
+    private void hookContextMenu() {
+        MenuManager menuMgr = new MenuManager("#PopupMenu");
+        menuMgr.setRemoveAllWhenShown(true);
+        menuMgr.addMenuListener(new IMenuListener() {
 
-			public void menuAboutToShow(IMenuManager manager) {
-				AlloyContentOutlinePage.this.fillContextMenu(manager);
-			}
+            public void menuAboutToShow(IMenuManager manager) {
+                AlloyContentOutlinePage.this.fillContextMenu(manager);
+            }
 
-		});
-		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-		getSite().registerContextMenu(null, menuMgr, viewer);
-	}
+        });
+        Menu menu = menuMgr.createContextMenu(viewer.getControl());
+        viewer.getControl().setMenu(menu);
+        getSite().registerContextMenu(null, menuMgr, viewer);
+    }
 
-	private void contributeToActionBars() {
-		IActionBars bars = getSite().getActionBars();
-		fillLocalPullDown(bars.getMenuManager());
-		fillLocalToolBar(bars.getToolBarManager());
-	}
+    private void contributeToActionBars() {
+        IActionBars bars = getSite().getActionBars();
+        fillLocalPullDown(bars.getMenuManager());
+        fillLocalToolBar(bars.getToolBarManager());
+    }
 
-	private void fillLocalPullDown(IMenuManager manager) {
-		manager.add(launchCommandAction);
-		// manager.add(new Separator());
-		manager.add(displayCommandAnswerAction);
-		manager.add(new Separator());
+    private void fillLocalPullDown(IMenuManager manager) {
+        manager.add(launchCommandAction);
+        // manager.add(new Separator());
+        manager.add(displayCommandAnswerAction);
+        manager.add(new Separator());
 
-	}
+    }
 
-	private void hookDoubleClickAction() {
-		viewer.addDoubleClickListener(new IDoubleClickListener() {
+    private void hookDoubleClickAction() {
+        viewer.addDoubleClickListener(new IDoubleClickListener() {
 
-			public void doubleClick(DoubleClickEvent event) {
-				Object selection = ((TreeSelection) event.getSelection())
-						.getFirstElement();
+            public void doubleClick(DoubleClickEvent event) {
+                Object selection = ((TreeSelection) event.getSelection())
+                        .getFirstElement();
 
-				if (selection instanceof IALSTreeDecorated) {
-					launchCommandAction.run();
-					// log.info("Selection mis � null");
-					// viewer.setSelection(null, true);
-				}
+                if (selection instanceof IALSTreeDecorated) {
+                    launchCommandAction.run();
+                    // log.info("Selection mis � null");
+                    // viewer.setSelection(null, true);
+                }
 
-				if (selection instanceof RootContent) {
-					if (viewer.getExpandedState(selection))
-						viewer.collapseToLevel(selection, -1);
-					else
-						viewer.expandToLevel(selection, 1);
+                if (selection instanceof RootContent) {
+                    if (viewer.getExpandedState(selection))
+                        viewer.collapseToLevel(selection, -1);
+                    else
+                        viewer.expandToLevel(selection, 1);
 
-				}
-			}
-		});
+                }
+            }
+        });
 
-	}
+    }
 
-	private void fillContextMenu(IMenuManager manager) {
-		manager.add(launchCommandAction);
-		// Other plug-ins can contribute there actions here
-		// manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-		manager.add(displayCommandAnswerAction);
-		// Other plug-ins can contribute there actions here
-		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-	}
+    private void fillContextMenu(IMenuManager manager) {
+        manager.add(launchCommandAction);
+        // Other plug-ins can contribute there actions here
+        // manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+        manager.add(displayCommandAnswerAction);
+        // Other plug-ins can contribute there actions here
+        manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+    }
 
-	private void fillLocalToolBar(IToolBarManager manager) {
-		manager.add(launchCommandAction);
-		manager.add(displayCommandAnswerAction);
-	}
+    private void fillLocalToolBar(IToolBarManager manager) {
+        manager.add(launchCommandAction);
+        manager.add(displayCommandAnswerAction);
+    }
 
-	private void makeActions() {
-		launchCommandAction = new LaunchCommandAction(viewer);
-		displayCommandAnswerAction = new DisplayCommandAnswerAction(viewer);
+    private void makeActions() {
+        launchCommandAction = new LaunchCommandAction(viewer);
+        displayCommandAnswerAction = new DisplayCommandAnswerAction(viewer);
 
-	}
+    }
 
-	public void setFocus() {
-		this.getControl().setFocus();
-	}
+    public void setFocus() {
+        this.getControl().setFocus();
+    }
 
-	class MySelectionListener implements ISelectionChangedListener {
+    class MySelectionListener implements ISelectionChangedListener {
 
-		public void selectionChanged(SelectionChangedEvent event) {
+        public void selectionChanged(SelectionChangedEvent event) {
 
-			Object selection = ((TreeSelection) event.getSelection())
-					.getFirstElement();
-			IALSTreeDecorated elem = (IALSTreeDecorated) selection;
-			IDocumentProvider provider = editor.getDocumentProvider();
-			IDocument document = provider.getDocument(editor.getEditorInput());
+            Object selection = ((TreeSelection) event.getSelection())
+                    .getFirstElement();
+            IDocumentProvider provider = editor.getDocumentProvider();
+            IDocument document = provider.getDocument(editor.getEditorInput());
 
-			int start = 0, end = 0;
-			
-			if (selection instanceof IALSCommand) {
-				IALSCommand command = (IALSCommand) selection;
-				boolean canShowCore = PreferenceConstants.V_SOLVER_MiniSatProverUnsatCore
-						.equals(AlloyPlugin.getDefault().getPreferenceStore()
-								.getString(PreferenceConstants.P_SOLVER_CHOICE));
-				
-				// If the chosen solver is MinisatProver Unsat Core and the
-				// result is UNSAT
-				if (canShowCore && command.shouldShowUnsatCore()) {
-					// show the core
-					Iterator<Pos> it = command.getCore().iterator();
-				    int temp = 0, x = 0, x2 = 0;
-					Pos pos;
-					if (it.hasNext()) {
-						pos = it.next();
-						try {
-							start = document.getLineOffset(pos.y);
-							x = pos.x;
-							x2=pos.x2;
-							end = start;
-						} catch (BadLocationException e) {
-							e.printStackTrace();
-						}
-					}
-					while (it.hasNext()) {
-						pos = it.next();
-						try {
-							temp = document.getLineOffset(pos.y);
-							if (temp <= start){
-								start = temp;
-								if (pos.x < x){
-									x = pos.x;
-								}
-								if (pos.x2 > x2){
-								    x2=pos.x2;
-								}
-							}
-							else if (temp > end)
-								end = temp;
-						} catch (BadLocationException e) {
-							e.printStackTrace();
-						}
-					}
-					editor.selectAndReveal(start - (x2 - x - 2), end - start + (x2 - x - 3));
-					
-				}
-				return;
-			}
-			
-			if (selection instanceof UnsatCorePos) {
-				UnsatCorePos ucp = (UnsatCorePos)selection;
-				try {
-					start = document.getLineOffset(elem.getBeginLine() - 1);
-					editor.selectAndReveal(start+ucp.getX() - 1 , ucp.getX2() - ucp.getX() + 1);
-				} catch (BadLocationException x) {
-					// ignore
-				}
-				return;
-			}	
-			
-			if (selection instanceof IALSTreeDecorated) {
-				try {
-					start = document.getLineOffset(elem.getBeginLine() - 1);
-					editor.selectAndReveal(start, 0);
-				} catch (BadLocationException x) {
-					// ignore
-				}
-				return;
-			}
-		}
-	}
+            int start = 0, end = 0;
+
+            if (selection instanceof IALSCommand) {
+                IALSCommand command = (IALSCommand) selection;
+                boolean canShowCore = PreferenceConstants.V_SOLVER_MiniSatProverUnsatCore
+                        .equals(AlloyPlugin.getDefault().getPreferenceStore()
+                                .getString(PreferenceConstants.P_SOLVER_CHOICE));
+
+                // If the chosen solver is MinisatProver Unsat Core and the
+                // result is UNSAT
+                if (canShowCore && command.shouldShowUnsatCore()) {
+                    // show the core
+                    Iterator<Pos> it = command.getCore().iterator();
+                    int temp = 0, x = 0, x2 = 0;
+                    Pos pos;
+                    if (it.hasNext()) {
+                        pos = it.next();
+                        try {
+                            start = document.getLineOffset(pos.y);
+                            x = pos.x;
+                            x2 = pos.x2;
+                            end = start;
+                        } catch (BadLocationException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    while (it.hasNext()) {
+                        pos = it.next();
+                        try {
+                            temp = document.getLineOffset(pos.y);
+                            if (temp <= start) {
+                                start = temp;
+                                if (pos.x < x) {
+                                    x = pos.x;
+                                }
+                                if (pos.x2 > x2) {
+                                    x2 = pos.x2;
+                                }
+                            } else if (temp > end)
+                                end = temp;
+                        } catch (BadLocationException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    editor.selectAndReveal(start - (x2 - x - 2), end - start
+                            + (x2 - x - 3));
+                    return;
+                }
+
+            }
+
+            if (selection instanceof IALSTreeDecorated) {
+                IALSTreeDecorated elem = (IALSTreeDecorated) selection;
+
+                if (selection instanceof UnsatCorePos) {
+                    UnsatCorePos ucp = (UnsatCorePos) selection;
+                    try {
+                        start = document.getLineOffset(elem.getBeginLine() - 1);
+                        editor.selectAndReveal(start + ucp.getX() - 1, ucp
+                                .getX2()
+                                - ucp.getX() + 1);
+                    } catch (BadLocationException x) {
+                        // ignore
+                    }
+                    return;
+                }
+
+                try {
+                    start = document.getLineOffset(elem.getBeginLine() - 1);
+                    editor.selectAndReveal(start, 0);
+                } catch (BadLocationException x) {
+                    // ignore
+                }
+            }
+        }
+    }
 
 }
