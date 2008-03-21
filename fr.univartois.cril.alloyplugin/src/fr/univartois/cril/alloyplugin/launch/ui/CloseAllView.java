9572064bb0a6fdb23525797d9397138e859fe9b1
rdruelle <rdruelle@751a51b9-0729-0410-b983-630554dc95c4>
2008-03-21 15:50:13 +0000

    Start issue 29: We have added a button on Outline page who close all views linked to all views. It also possible to select one command by doing a right-click and click on close view in popup menu in order to close the view linked to this command.

---
+++ fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/launch/ui/CloseAllView.java
@@ -0,0 +1,109 @@
+package fr.univartois.cril.alloyplugin.launch.ui;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.ui.actions.SelectionProviderAction;
+
+import fr.univartois.cril.alloyplugin.api.ALSImageRegistry;
+import fr.univartois.cril.alloyplugin.api.IALSCommand;
+import fr.univartois.cril.alloyplugin.api.IALSFile;
+import fr.univartois.cril.alloyplugin.api.IAlloyTreeContentProvider;
+
+/**
+ * 
+ * @author romuald druelle
+ * @author lionel desruelles
+ * 
+ */
+public class CloseAllView extends SelectionProviderAction {
+
+	private static final ImageDescriptor iconclose = ALSImageRegistry
+			.getImageDescriptor(ALSImageRegistry.CLOSE_GRAPH_ICON_ID);
+	private static final String ACTION_ID = "fr.univartois.cril.alloyplugin.launch.closeallview";
+
+	private static final String TEXT_CLOSE_ALL_VIEWS = "Close All Views";
+
+	private static final String TEXT_CLOSE_ONE_VIEW = "Close View";
+
+	private IALSFile file;
+
+	private TreeViewer viewer;
+
+	private IStructuredSelection selection;
+
+	private boolean commandRootSelected;
+
+	private IALSCommand ialsc;
+
+	public CloseAllView(TreeViewer provider, IALSFile file) {
+		super(provider, TEXT_CLOSE_ONE_VIEW);
+		viewer = provider;
+		this.file = file;
+		this.setEnabled(true);
+		setImageDescriptor(iconclose);
+		setToolTipText("Close view");
+		setActionDefinitionId(ACTION_ID);
+	}
+
+	public IALSFile getFile() {
+		return this.file;
+	}
+
+	public void selectionChanged(IStructuredSelection selection) {
+		commandRootSelected = false;
+		this.selection = selection;
+		if (selection.isEmpty())
+			commandRootSelected = true;
+		else {
+			for (Iterator iter = selection.iterator(); iter.hasNext();) {
+				Object o = iter.next();
+				IAlloyTreeContentProvider contentProvider = (IAlloyTreeContentProvider) viewer
+						.getContentProvider();
+				if (o == contentProvider.getCommandsRootContent()) {
+
+					if (contentProvider.getChildren(o).length >= 1) {
+						this.setEnabled(true);
+						commandRootSelected = true;
+						setText(TEXT_CLOSE_ALL_VIEWS);
+
+						return;
+					}
+				} else if (o instanceof IALSCommand) {
+					setText(TEXT_CLOSE_ONE_VIEW);
+					ialsc = (IALSCommand) o;
+					this.setEnabled(true);
+					return;
+				}
+
+			}
+
+		}
+		setText(TEXT_CLOSE_ONE_VIEW);
+		commandRootSelected = true;
+	}
+
+	public void run() {
+		List<IALSCommand> commandes = getFile().getCommand();
+		if (commandRootSelected) {
+			for (IALSCommand c : commandes) {
+				c.closeYourView();
+			}
+		} else {
+			for (IALSCommand c : commandes) {
+				if (ialsc.equals(c)) {
+					c.closeYourView();
+				}
+			}
+		}
+
+	}
+
+}
