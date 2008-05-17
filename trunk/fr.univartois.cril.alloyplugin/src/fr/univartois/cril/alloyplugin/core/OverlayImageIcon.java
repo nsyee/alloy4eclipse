51607e8f8ba36eb3ebded95999a56a5d12e981c3
rdruelle <rdruelle@751a51b9-0729-0410-b983-630554dc95c4>
2008-05-17 09:05:23 +0000

    - In preferences page you can choose to close the previous solution/counter-example when the answer becomes UNSAT. Before you had to check the box "Remove old VizView when an assertion has no longer a counter-example". This message has been replaced by a more explicit message and now you have to check the box "Close previous solution/counter-example when the answer becomes UNSAT". - Adding a few comments concerning explanation decorators in class OverlayImageIcon.java

--- trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/core/OverlayImageIcon.java
+++ trunk/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/core/OverlayImageIcon.java
@@ -12,12 +12,24 @@ import org.eclipse.swt.graphics.Point;
  */
 public class OverlayImageIcon extends CompositeImageDescriptor {
 
+	/**
+	 * Base image of the object
+     */ 
 	private Image baseImage;
 
+	/**
+     * Size of the base image 
+     */ 
 	private Point sizeOfImage;
 
+	/**
+	 * Decorator image
+	 */
 	private Image decoratorImage;
 
+	/**
+	 * Constructor for overlayImageIcon.
+     */
 	public OverlayImageIcon(Image baseImage, Image decoratorImage) {
 		this.baseImage = baseImage;
 		this.decoratorImage = decoratorImage;
@@ -26,25 +38,46 @@ public class OverlayImageIcon extends CompositeImageDescriptor {
 	}
 
 	protected void drawCompositeImage(int arg0, int arg1) {
+		// draw the base image
 		drawImage(getBaseImage().getImageData(), 0, 0);
 		ImageData imageData = getDecoratorImage().getImageData();
+		// draw the decorator image on bottom right corner
 		drawImage(imageData, sizeOfImage.x - imageData.width, sizeOfImage.y
 				- imageData.height);
 
 	}
 
+    /**
+     * Get the size of the base image
+     * @return Point
+     */
 	protected Point getSize() {
         return sizeOfImage;
 	}
     
+	/**
+	 * Get the base image
+	 * 
+	 * @return Image
+	 */
 	protected Image getBaseImage(){
 		return this.baseImage;
 	}
-	
+
+	/**
+	 * Get the decorator image
+	 * 
+	 * @return Image
+	 */
 	protected Image getDecoratorImage(){
 		return this.decoratorImage;
 	}
 	
+	/**
+	  * Get the image formed by overlaying the decorator image on the base image
+	  * 
+	  * @return composite image
+     */ 
 	public Image getImage() {
 		return createImage();
 	}
