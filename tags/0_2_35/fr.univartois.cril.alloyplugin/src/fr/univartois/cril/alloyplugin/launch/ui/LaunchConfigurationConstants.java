9a074e466c276170fe0163628232939cb5247685
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2008-12-28 07:58:47 +0000

    We are now creating a tag for each release of A4E, to facilitate debugging.

---
+++ tags/0_2_35/fr.univartois.cril.alloyplugin/src/fr/univartois/cril/alloyplugin/launch/ui/LaunchConfigurationConstants.java
@@ -0,0 +1,25 @@
+package fr.univartois.cril.alloyplugin.launch.ui;
+
+public class LaunchConfigurationConstants {
+	/**
+	 * Attribute for a list of the commands to be executed.
+	 */
+	protected static final String ATTRIBUTE_COMMANDS_LABEL_LIST = "commands_list";
+	/**
+	 * Attribute for the location of the .als file where commands are stored.
+	 * 
+	 */
+	protected static final String ATTRIBUTE_FILE_NAME = "attribut_filename";
+	
+	public static final String LAUNCH_CONFIGURATION_TYPE="fr.univartois.cril.alloyplugin.launch.ui.LaunchConfigurationType";
+	public static final String CHOOSE_CONFIGURATION_TITLE = "choose a configuration";
+	public static final String CHOOSE_CONFIGURATION_MESSAGE = "&Select existing configuration";
+	public static final String RUN_MODE = "run";
+	/**
+	 * Attribute for a quick config. 
+	 * 
+	 */
+	public static final String ATTRIBUTE_QUICK_CONFIG = "quick_config_attribute";
+
+	
+}
