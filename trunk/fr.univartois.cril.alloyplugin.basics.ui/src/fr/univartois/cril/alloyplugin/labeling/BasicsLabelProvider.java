71509107240892b721213811c7835ddfc968bc7b
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2010-04-29 09:04:11 +0000

    Updated the classes used in the outline view.

--- trunk/fr.univartois.cril.alloyplugin.basics.ui/src/fr/univartois/cril/alloyplugin/labeling/BasicsLabelProvider.java
+++ trunk/fr.univartois.cril.alloyplugin.basics.ui/src/fr/univartois/cril/alloyplugin/labeling/BasicsLabelProvider.java
@@ -5,15 +5,15 @@ package fr.univartois.cril.alloyplugin.labeling;
 
 import org.eclipse.xtext.ui.core.DefaultLabelProvider;
 
-import fr.univartois.cril.alloyplugin.basics.AssertDecl;
-import fr.univartois.cril.alloyplugin.basics.CheckDecl;
+import fr.univartois.cril.alloyplugin.basics.Assertion;
+import fr.univartois.cril.alloyplugin.basics.CheckCommand;
 import fr.univartois.cril.alloyplugin.basics.EnumDecl;
-import fr.univartois.cril.alloyplugin.basics.FactDecl;
-import fr.univartois.cril.alloyplugin.basics.FunDecl;
+import fr.univartois.cril.alloyplugin.basics.Fact;
+import fr.univartois.cril.alloyplugin.basics.Function;
 import fr.univartois.cril.alloyplugin.basics.Module;
-import fr.univartois.cril.alloyplugin.basics.PredDecl;
-import fr.univartois.cril.alloyplugin.basics.RunDecl;
-import fr.univartois.cril.alloyplugin.basics.SigDecl;
+import fr.univartois.cril.alloyplugin.basics.Predicate;
+import fr.univartois.cril.alloyplugin.basics.RunCommand;
+import fr.univartois.cril.alloyplugin.basics.Signature;
 
 /**
  * see
@@ -31,42 +31,74 @@ public class BasicsLabelProvider extends DefaultLabelProvider {
 	/*
 	 * String text(Module module) { return module.getName().getName(); }
 	 */
-	/*String text(Module module){
-	
-	}*/
+	String text(Module module){
+		return module.getModuleName().getName();
+	}
 
 	String image(Module module) {
 		return "module.gif";
 	}
 
-	String image(SigDecl sig) {
+	String text(Signature sig){
+		return sig.getSigName().getName();
+	}
+	
+	String image(Signature sig) {
 		return "sig.gif";
 	}
 
-	String image(FactDecl fact) {
+	String text(Fact fact){
+		return fact.getFactName().getName();
+	}
+	
+	String image(Fact fact) {
 		return "fact.gif";
 	}
 
-	String image(AssertDecl assertD) {
+	String text(Assertion assertD){
+		return assertD.getAssertName().getName();
+	}
+	
+	String image(Assertion assertD) {
 		return "assert.gif";
 	}
 
-	String image(FunDecl fun) {
+	String text(Function fun){
+		return fun.getFunName().getName();
+	}
+	
+	String image(Function fun) {
 		return "function.gif";
 	}
 
-	String image(PredDecl fun) {
+	String text(Predicate fun){
+		return fun.getPredName().getName();
+	}
+	
+	String image(Predicate fun) {
 		return "predicate.gif";
 	}
 
-	String image(RunDecl run) {
+	String text(RunCommand run){
+		return run.getRunName().getName();
+	}
+	
+	String image(RunCommand run) {
 		return "run.gif";
 	}
-
-	String image(CheckDecl check) {
+	
+	String text(CheckCommand check){
+		return check.getCheckName().getName();
+	}
+	
+	String image(CheckCommand check) {
 		return "check.gif";
 	}
 
+	String text(EnumDecl enumD){
+		return enumD.getEnumName().getName();
+	}
+	
 	String image(EnumDecl enumD) {
 		return "sig.gif";
 	}
