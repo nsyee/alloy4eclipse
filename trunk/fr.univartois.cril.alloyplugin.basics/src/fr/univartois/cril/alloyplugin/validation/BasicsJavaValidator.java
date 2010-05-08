0364ea921b315ecbcf64c9b195d2cf56a892c77e
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2010-05-08 14:36:32 +0000

    Fixed the issue with 0/1 after expects. Probably require a specific scoping to have only 0 or 1 with content assist.

--- trunk/fr.univartois.cril.alloyplugin.basics/src/fr/univartois/cril/alloyplugin/validation/BasicsJavaValidator.java
+++ trunk/fr.univartois.cril.alloyplugin.basics/src/fr/univartois/cril/alloyplugin/validation/BasicsJavaValidator.java
@@ -1,13 +1,18 @@
 package fr.univartois.cril.alloyplugin.validation;
+
+import org.eclipse.xtext.validation.Check;
+
+import fr.univartois.cril.alloyplugin.basics.BasicsPackage;
+import fr.univartois.cril.alloyplugin.basics.Expectation;
  
 
 public class BasicsJavaValidator extends AbstractBasicsJavaValidator {
 
-//	@Check
-//	public void checkTypeNameStartsWithCapital(Type type) {
-//		if (!Character.isUpperCase(type.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
-//		}
-//	}
+	@Check
+	public void checkTypeNameStartsWithCapital(Expectation exp) {
+		if (!(exp.getValue()==0)&&!(exp.getValue()==1)) {
+			error("expectation must be 0 or 1", BasicsPackage.EXPECTATION__VALUE);
+		}
+	}
 
 }
