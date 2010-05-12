bdb433155e52c88c41c702930bed0ed3218d4501
simo.said09 <simo.said09@751a51b9-0729-0410-b983-630554dc95c4>
2010-05-12 18:08:26 +0000

    update for BasicsUiModule.java

--- fr.univartois.cril.alloyplugin.basics.ui/src/fr/univartois/cril/alloyplugin/BasicsUiModule.java
+++ fr.univartois.cril.alloyplugin.basics.ui/src/fr/univartois/cril/alloyplugin/BasicsUiModule.java
@@ -3,9 +3,32 @@
  */
 package fr.univartois.cril.alloyplugin;
 
+import org.eclipse.xtext.ui.common.editor.outline.actions.IContentOutlineNodeAdapterFactory;
+import org.eclipse.xtext.ui.common.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.ui.common.editor.syntaxcoloring.ISemanticHighlightingConfiguration;
+import org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.AbstractAntlrTokenToAttributeIdMapper;
+import org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.DefaultAntlrTokenToAttributeIdMapper;
+
+import fr.univartois.cril.alloyplugin.outline.BasicsOutlineNodeAdapterFactory;
+
 /**
  * Use this class to register components to be used within the IDE.
  */
-public class BasicsUiModule extends fr.univartois.cril.alloyplugin.AbstractBasicsUiModule {
+public class BasicsUiModule extends
+		fr.univartois.cril.alloyplugin.AbstractBasicsUiModule {
+	public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator() {
+		return SemanticHighlightingCalculator.class;
+	}
 
+	public Class<? extends ISemanticHighlightingConfiguration> bindISemanticHighlightingConfiguration() {
+		return SemanticHighlightingConfiguration.class;
+	}
+
+//	public Class<? extends AbstractAntlrTokenToAttributeIdMapper> bindTokenToAttributeIdMapper() {
+//		return DefaultAntlrTokenToAttributeIdMapper.class;
+//	}
+//	
+//	public Class<? extends IContentOutlineNodeAdapterFactory> bindIContentOutlineAdapterFactory() {
+//		return BasicsOutlineNodeAdapterFactory.class;
+//	}
 }
