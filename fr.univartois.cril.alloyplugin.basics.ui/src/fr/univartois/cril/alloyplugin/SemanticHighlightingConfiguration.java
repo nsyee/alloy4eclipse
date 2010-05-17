702dbcc1cf68694b2fb2c5929bac3d29ce2a0a30
simo.said09 <simo.said09@751a51b9-0729-0410-b983-630554dc95c4>
2010-05-17 07:00:15 +0000

    update for SemanticHighlightingConfiguration.java

--- fr.univartois.cril.alloyplugin.basics.ui/src/fr/univartois/cril/alloyplugin/SemanticHighlightingConfiguration.java
+++ fr.univartois.cril.alloyplugin.basics.ui/src/fr/univartois/cril/alloyplugin/SemanticHighlightingConfiguration.java
@@ -12,14 +12,63 @@ public class SemanticHighlightingConfiguration implements
 	public final static String signatureName = "SignatureName";
 	public final static String proprityName = "proprityName";
 	public final static String referenceName = "referenceName";
+	public final static String predicateName = "predicateName";
+	public final static String assertionName = "assertionName";
+	public final static String factName= "factName";
+	public final static String functionName = "functionName";
+	public final static String enumName = "enumName";
+	public final static String enumPropertyName = "enumPropertyName";
 	
-
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
 		acceptor.acceptDefaultHighlighting(signatureName, "Signature name", signatureNameStyle());
 		acceptor.acceptDefaultHighlighting(proprityName, "Proprity name", proprityNameStyle());
 		acceptor.acceptDefaultHighlighting(referenceName, "Reference Name", referenceNameStyle());
+		acceptor.acceptDefaultHighlighting(predicateName, "Predicate Name", predicateNameStyle());
+		acceptor.acceptDefaultHighlighting(assertionName, "Assertion Name", assertionNameStyle());
+		acceptor.acceptDefaultHighlighting(factName, "Fact Name", factNameStyle());
+		acceptor.acceptDefaultHighlighting(functionName, "Function Name", functionNameStyle());
+		acceptor.acceptDefaultHighlighting(enumName, "Enum Name", enumNameStyle());
+		acceptor.acceptDefaultHighlighting(enumPropertyName, "Enum Property Name", enumPropertyNameStyle());
 	}
-
+	public TextStyle assertionNameStyle() {
+		TextStyle textStyle = new TextStyle();
+		textStyle.setBackgroundColor(new RGB(255, 255, 255));
+		textStyle.setColor(new RGB(34, 122, 243));
+		textStyle.setStyle(SWT.ITALIC);
+		return textStyle; 
+	}
+	public TextStyle factNameStyle() {
+		TextStyle textStyle = new TextStyle();
+		textStyle.setBackgroundColor(new RGB(255, 255, 255));
+		textStyle.setColor(new RGB(1, 132, 43));
+		textStyle.setStyle(SWT.ITALIC);
+		return textStyle; 
+	}public TextStyle functionNameStyle() {
+		TextStyle textStyle = new TextStyle();
+		textStyle.setBackgroundColor(new RGB(255, 255, 255));
+		textStyle.setColor(new RGB(4, 12, 243));
+		textStyle.setStyle(SWT.BOLD);
+		return textStyle; 
+	}public TextStyle enumNameStyle() {
+		TextStyle textStyle = new TextStyle();
+		textStyle.setBackgroundColor(new RGB(255, 255, 255));
+		textStyle.setColor(new RGB(234, 1, 13));
+		textStyle.setStyle(SWT.ITALIC);
+		return textStyle; 
+	}public TextStyle enumPropertyNameStyle() {
+		TextStyle textStyle = new TextStyle();
+		textStyle.setBackgroundColor(new RGB(255, 255, 255));
+		textStyle.setColor(new RGB(0, 1, 243));
+		textStyle.setStyle(SWT.ITALIC);
+		return textStyle; 
+	}
+	public TextStyle predicateNameStyle() {
+		TextStyle textStyle = new TextStyle();
+		textStyle.setBackgroundColor(new RGB(255, 255, 255));
+		textStyle.setColor(new RGB(234, 12, 143));
+		textStyle.setStyle(SWT.ITALIC);
+		return textStyle; 
+	}
 	public TextStyle signatureNameStyle() {
 		TextStyle textStyle = new TextStyle();
 		textStyle.setBackgroundColor(new RGB(255, 255, 255));
