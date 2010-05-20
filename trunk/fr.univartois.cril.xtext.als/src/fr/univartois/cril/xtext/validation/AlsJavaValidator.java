f23cf58173c28618c463f58183cf10e370daf39d
bouragbamohamed <bouragbamohamed@751a51b9-0729-0410-b983-630554dc95c4>
2010-05-20 10:04:26 +0000

    Update of validation

--- trunk/fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/validation/AlsJavaValidator.java
+++ trunk/fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/validation/AlsJavaValidator.java
@@ -1,9 +1,21 @@
 package fr.univartois.cril.xtext.validation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.validation.Check;
 
 import fr.univartois.cril.xtext.als.AlsPackage;
 import fr.univartois.cril.xtext.als.Expectation;
+import fr.univartois.cril.xtext.als.Paragraph;
+import fr.univartois.cril.xtext.als.Predicate;
+import fr.univartois.cril.xtext.als.PredicateName;
+import fr.univartois.cril.xtext.als.PropertyName;
+import fr.univartois.cril.xtext.als.Signature;
+import fr.univartois.cril.xtext.als.SignatureName;
+import fr.univartois.cril.xtext.als.Specification;
  
 
 public class AlsJavaValidator extends AbstractAlsJavaValidator {
@@ -19,6 +31,50 @@ public class AlsJavaValidator extends AbstractAlsJavaValidator {
 	public void checkTypeNameStartsWithCapital(Expectation exp) {
 		if (!(exp.getValue()==0)&&!(exp.getValue()==1)) {
 			error("expectation must be 0 or 1", AlsPackage.EXPECTATION__VALUE);
+		}
+	}
+	
+	@Check
+	public void checkUniqueSigName(SignatureName sig){
+		Specification specification ;
+		EObject object = EcoreUtil.getRootContainer(sig);
+		if(!(object instanceof Specification)) return;
+		specification = (Specification)object;
+		
+		for(Paragraph paragraph : specification.getParagraph()){
+			if(paragraph == null) continue;
+			if(paragraph instanceof Signature){
+				Signature signature =(Signature) paragraph;
+				for(SignatureName s : signature.getSignatureName() ){
+					if(s == null || s ==sig) continue;
+					if(s.getName().equals(sig.getName())){
+						error("Duplicate name : "+ sig.getName(), AlsPackage.SIGNATURE__SIGNATURE_NAME);
+						return ;
+					}
+				}
+			}
+		}
+		
+	}
+	
+	@Check
+	public void checkUniquePredName(PredicateName pred){
+		Specification specification ;
+		EObject object = EcoreUtil.getRootContainer(pred);
+		if(!(object instanceof Specification)) return;
+		specification = (Specification)object;
+		
+		for(Paragraph paragraph : specification.getParagraph()){
+			if(paragraph == null) continue;
+			if(paragraph instanceof Predicate){
+				Predicate predicate =(Predicate) paragraph;
+				PredicateName p = predicate.getPredicateName();
+				if(p == null || p ==pred) continue;
+				if(p.getName().equals(pred.getName())){
+					error("Duplicate name : "+ pred.getName(), AlsPackage.PREDICATE__PREDICATE_NAME);
+					return ;
+				}
+			}
 		}
 	}
 
