bc27a55863439d119800961c54b16ce940452d57
vasseur.yoann <vasseur.yoann@751a51b9-0729-0410-b983-630554dc95c4>
2011-04-11 15:29:38 +0000

    Added models at the A4E in order to add the open command

---
+++ trunk/fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/als/uri/AlsImportUriGlobalScopeProvider.java
@@ -0,0 +1,19 @@
+package fr.univartois.cril.xtext.als.uri;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.scoping.IScope;
+import org.eclipse.xtext.scoping.impl.ImportUriGlobalScopeProvider;
+
+
+public class AlsImportUriGlobalScopeProvider  extends ImportUriGlobalScopeProvider{
+
+	@Override
+	public IScope getScope(EObject context, EReference reference) {
+		
+		
+
+		return super.getScope(context, reference);
+	}
+	
+}
