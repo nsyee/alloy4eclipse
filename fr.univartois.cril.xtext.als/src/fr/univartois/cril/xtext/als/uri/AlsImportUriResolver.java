6422185973dd4cf0ec39b9ef508fbbafa662b010
vasseur.yoann <vasseur.yoann@751a51b9-0729-0410-b983-630554dc95c4>
2011-04-12 07:10:08 +0000

    fixed troubles with open directives. Only working with models at the current time.

--- fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/als/uri/AlsImportUriResolver.java
+++ fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/als/uri/AlsImportUriResolver.java
@@ -3,18 +3,14 @@ package fr.univartois.cril.xtext.als.uri;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.scoping.impl.ImportUriResolver;
 
-
-
 public class AlsImportUriResolver extends ImportUriResolver {
 
-	
 	@Override
 	public String resolve(EObject object) {
 		
-			
-		
-		 return "platform:/resource/nomPrjoet/models/"+ super.resolve(object)+".als";
+		return "platform:/resource/"+object.eResource()
+		.getURI()
+		.segment(object.eResource().getURI().segmentCount() - 2)+"/models/" + super.resolve(object) + ".als";
 	}
-	
 
 }
