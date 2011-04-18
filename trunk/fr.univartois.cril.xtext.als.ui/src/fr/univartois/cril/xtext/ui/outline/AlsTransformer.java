b8094603c15eb27adabe76de4abd6e8ad3f1cc73
vasseur.yoann <vasseur.yoann@751a51b9-0729-0410-b983-630554dc95c4>
2011-04-18 10:08:32 +0000

    Try to resolve images jar problems

--- trunk/fr.univartois.cril.xtext.als.ui/src/fr/univartois/cril/xtext/ui/outline/AlsTransformer.java
+++ trunk/fr.univartois.cril.xtext.als.ui/src/fr/univartois/cril/xtext/ui/outline/AlsTransformer.java
@@ -10,6 +10,15 @@ import org.eclipse.xtext.ui.editor.outline.ContentOutlineNode;
 import org.eclipse.xtext.ui.editor.outline.actions.IActionBarContributor;
 import org.eclipse.xtext.ui.editor.outline.transformer.AbstractDeclarativeSemanticModelTransformer;
 
+import fr.univartois.cril.xtext.als.Assertion;
+import fr.univartois.cril.xtext.als.CheckCommand;
+import fr.univartois.cril.xtext.als.Fact;
+import fr.univartois.cril.xtext.als.Function;
+import fr.univartois.cril.xtext.als.Module;
+import fr.univartois.cril.xtext.als.Open;
+import fr.univartois.cril.xtext.als.Predicate;
+import fr.univartois.cril.xtext.als.RunCommand;
+import fr.univartois.cril.xtext.als.Signature;
 import fr.univartois.cril.xtext.als.Specification;
 
 /**
@@ -29,5 +38,70 @@ public class AlsTransformer extends AbstractDeclarativeSemanticModelTransformer 
 	public String createNode(Specification semanticNode, ContentOutlineNode parentNode) {	    
 	    return INVISIBLE_ROOT_NODE;
 	}
+	public ContentOutlineNode createNode(Module moduleNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(moduleNode, parentNode);
+		    node.setLabel("Module : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(Open openNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(openNode, parentNode);
+		    node.setLabel("Open : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(Function fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Function : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(Signature fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Signature : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(Predicate fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Predicat : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(Assertion fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Assertion : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(RunCommand fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Run : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(CheckCommand fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Check : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	public ContentOutlineNode createNode(Fact fNode, ContentOutlineNode parentNode) {	    
+		 ContentOutlineNode node = super.newOutlineNode(fNode, parentNode);
+		    node.setLabel("Fact : " + node.getLabel());
+		    node.setImage(node.getImage());
+		    return node;
+		
+	}
+	
+	
 	
 }
