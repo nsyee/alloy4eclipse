00d0f72983abdab9f6df129ac191ad6fd151b646
kapusta.maxime <kapusta.maxime@751a51b9-0729-0410-b983-630554dc95c4>
2011-05-17 06:50:12 +0000

    Commands are now fully functional. Due to a little change in the outline handler, they were no more found in the parsed file. The change has been removed for the moment.

--- fr.univartois.cril.xtext.als.ui/src/fr/univartois/cril/xtext/ui/editor/outline/CommandOutlineNodeHandler.java
+++ fr.univartois.cril.xtext.als.ui/src/fr/univartois/cril/xtext/ui/editor/outline/CommandOutlineNodeHandler.java
@@ -1,6 +1,5 @@
 package fr.univartois.cril.xtext.ui.editor.outline;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -12,7 +11,6 @@ import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.XtextDocumentUtil;
-import org.eclipse.xtext.ui.editor.outline.ContentOutlineNode;
 import org.eclipse.xtext.ui.editor.utils.EditorUtils;
 
 import edu.mit.csail.sdg.alloy4.Err;
@@ -23,21 +21,18 @@ import fr.univartois.cril.xtext.alloyplugin.api.IReporter;
 import fr.univartois.cril.xtext.alloyplugin.core.ALSFile;
 import fr.univartois.cril.xtext.alloyplugin.core.ExecutableCommand;
 import fr.univartois.cril.xtext.alloyplugin.core.Reporter;
-import fr.univartois.cril.xtext.ui.outline.AlsTransformer;
-import fr.univartois.cril.xtext.ui.outline.CommandOutlineNode;
 
 public class CommandOutlineNodeHandler extends AbstractHandler {
-	
-	 List<CommandOutlineNode> nodes=AlsTransformer.getNodes();
-	
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
+		System.out.println("TEST!!");
 		IXtextDocument document;
 		XtextEditor editor;
 		IResource resource;
 		ALSFile file;
 		int line, offset;
 		Command command;
+		
 		editor = EditorUtils.getActiveXtextEditor(event);
 		if (editor == null)
 			return null;
@@ -61,59 +56,12 @@ public class CommandOutlineNodeHandler extends AbstractHandler {
 		int index = retrieveIndexOfCommand(world.getAllCommands(), command);
 		if (index == -1)
 			return null;
-		
-		ExecutableCommand ex=null;//=new ExecutableCommand(file,command,index,world);
-		System.out.println("command ici:"+command);
-		for(CommandOutlineNode node: nodes){
-			System.out.println("commande dans les noeuds:"+node.getCommand());
-			if(command.toString().equals(node.getCommand().toString())){
-				ex=node.getCommand();
-			}
-		}
-		if(ex==null){
-			System.out.println("RETOURNE NULL");
-			return null;
-		}
-		else{
-			executeCommand(ex, reporter, null);
-		}
+		ExecutableCommand ex = new ExecutableCommand(file, command, index,
+				world);
+		executeCommand(ex, reporter, null);
 		return null;
 	}
 
-	public ExecutableCommand getExecutableCommand(ContentOutlineNode ct){
-		IXtextDocument document;
-		XtextEditor editor;
-		IResource resource;
-		ALSFile file;
-		int line, offset;
-		Command command;
-		
-		editor = EditorUtils.getActiveXtextEditor();
-		if (editor == null)
-			return null;
-		document = XtextDocumentUtil.get(editor);
-		if(editor.isSaveOnCloseNeeded()) return null;
-		resource = editor.getResource();
-		
-		offset = ct.getSelectionOffset();
-		line = getLine(document, offset) + 1;
-		if (line == -1)
-			return null;
-		List<Command> list = getList(resource);
-		command = retrieveCommand(list, line);
-		file = new ALSFile(resource);
-		CompModule world;
-		String filename = file.getFilename();
-		IReporter reporter = new Reporter(resource);
-		world = getWorld(reporter, filename);
-		if (world == null)
-			return null;
-		int index = retrieveIndexOfCommand(world.getAllCommands(), command);
-		if (index == -1)
-			return null;
-		ExecutableCommand ex=new ExecutableCommand(file,command,index,world);
-		return ex;
-	}
 	private int getLine(IXtextDocument document, int offset) {
 		int line = -1;
 		try {
@@ -174,9 +122,4 @@ public class CommandOutlineNodeHandler extends AbstractHandler {
 			// TODO Auto-generated catch block
 		}
 	}
-
-	public void addNode(CommandOutlineNode node){
-		nodes.add(node);
-	}
-
 }
