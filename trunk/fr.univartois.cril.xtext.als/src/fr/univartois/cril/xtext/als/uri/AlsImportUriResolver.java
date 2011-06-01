ee6f139ec799b11afed0df38f7daec98b0abb029
vasseur.yoann <vasseur.yoann@751a51b9-0729-0410-b983-630554dc95c4>
2011-06-01 08:35:41 +0000

    add "super folder" containing the sample models for all projects.

--- trunk/fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/als/uri/AlsImportUriResolver.java
+++ trunk/fr.univartois.cril.xtext.als/src/fr/univartois/cril/xtext/als/uri/AlsImportUriResolver.java
@@ -1,5 +1,8 @@
 package fr.univartois.cril.xtext.als.uri;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.scoping.impl.ImportUriResolver;
 
@@ -7,16 +10,25 @@ public class AlsImportUriResolver extends ImportUriResolver {
 
 	@Override
 	public String resolve(EObject object) {
-		
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+
 		if (super.resolve(object) != null) {
-			
+			IProject myProject = root.getProject(object.eResource().getURI()
+					.segment(1));
+		
+			if (myProject.getFile("/" + super.resolve(object) + ".als").exists()) {
+				String location = "platform:/resource/"
+						+ object.eResource().getURI().segment(1) + "/"
+						+ super.resolve(object) + ".als";
+				
+				return location;
+			}
 			String location = "platform:/resource/"
-				+ object.eResource()
-				.getURI()
-				.segment(1)
-		+ "/" + super.resolve(object) + ".als";
+				+ "Sample Models" + "/"
+				+ super.resolve(object) + ".als";
 			
 			return location;
+			
 		}
 		return null;
 	}
