da1120fe7ac94f03e709e942afdd5d19729c6845
daniel.leberre <daniel.leberre@751a51b9-0729-0410-b983-630554dc95c4>
2012-04-19 15:47:43 +0000

    Fixed Hover code for signature.

--- trunk/fr.univartois.cril.xtext2.ui/src/fr/univartois/cril/xtext2/ui/javadoc/AlsEObjectDocumentationProvider.java
+++ trunk/fr.univartois.cril.xtext2.ui/src/fr/univartois/cril/xtext2/ui/javadoc/AlsEObjectDocumentationProvider.java
@@ -3,14 +3,13 @@ package fr.univartois.cril.xtext2.ui.javadoc;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.documentation.IEObjectDocumentationProvider;
 
-import fr.univartois.cril.xtext2.als.Signature;
+import fr.univartois.cril.xtext2.als.SignatureName;
 
 public class AlsEObjectDocumentationProvider implements IEObjectDocumentationProvider {
 	
-	    @Override
 	    public String getDocumentation(EObject o) {
-	    	System.out.println("Bonjour") ;	    	
-	        if (o instanceof Signature) {
+	    	System.out.println("Bonjour"+o) ;	    	
+	        if (o instanceof SignatureName) {
 	            return "This is a nice Greeting with nice <b>markup</b> in the <i>documentation</i>";
 	        }
 	        return null;
