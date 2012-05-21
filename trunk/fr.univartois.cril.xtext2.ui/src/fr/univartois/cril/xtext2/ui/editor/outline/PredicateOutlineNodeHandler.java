25f34b87f2cb6eb24b88d85814682796c5a3eca6
hetman.freddy <hetman.freddy@751a51b9-0729-0410-b983-630554dc95c4>
2012-05-21 16:12:53 +0000

    launch delegate for predicate, module and command

--- trunk/fr.univartois.cril.xtext2.ui/src/fr/univartois/cril/xtext2/ui/editor/outline/PredicateOutlineNodeHandler.java
+++ trunk/fr.univartois.cril.xtext2.ui/src/fr/univartois/cril/xtext2/ui/editor/outline/PredicateOutlineNodeHandler.java
@@ -6,102 +6,49 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.XtextDocumentUtil;
 import org.eclipse.xtext.ui.editor.utils.EditorUtils;
 
-import edu.mit.csail.sdg.alloy4.ConstList;
-import edu.mit.csail.sdg.alloy4.Err;
-import edu.mit.csail.sdg.alloy4.Pair;
-import edu.mit.csail.sdg.alloy4.SafeList;
-import edu.mit.csail.sdg.alloy4compiler.ast.Command;
-import edu.mit.csail.sdg.alloy4compiler.ast.Decl;
-import edu.mit.csail.sdg.alloy4compiler.ast.Expr;
-import edu.mit.csail.sdg.alloy4compiler.ast.Func;
-import edu.mit.csail.sdg.alloy4compiler.ast.Module;
-import edu.mit.csail.sdg.alloy4compiler.parser.CompModule;
-import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil;
-import fr.univartois.cril.xtext2.alloyplugin.api.IReporter;
-import fr.univartois.cril.xtext2.alloyplugin.core.ALSFile;
-import fr.univartois.cril.xtext2.alloyplugin.core.ExecutableCommand;
-import fr.univartois.cril.xtext2.alloyplugin.core.Reporter;
-import fr.univartois.cril.xtext2.preferences.PreferenceConstants;
-import fr.univartois.cril.xtext2.ui.activator.AlsActivator;
+import fr.univartois.cril.xtext2.alloyplugin.api.ALSImageRegistry;
+import fr.univartois.cril.xtext2.alloyplugin.launch.ui.LaunchConfigurationConstants;
+import fr.univartois.cril.xtext2.alloyplugin.launch.ui.LaunchQuickConfigFactory;
 
 public class PredicateOutlineNodeHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IXtextDocument document;
-		XtextEditor editor;
-		IResource resource;
-		ALSFile file;
-		int line, offset;
-		CompModule world;
-		String content = null, predName;
-		Command command = null;
-
-		editor = EditorUtils.getActiveXtextEditor(event);
-		if (editor == null)
-			return null;
-		document = XtextDocumentUtil.get(editor);
-
-		if (editor.isSaveOnCloseNeeded())
-			return null;
-		resource = editor.getResource();
-
-		offset = editor.getHighlightRange().getOffset();
-		line = getLine(document, offset) + 1;
-		if (line == -1)
-			return null;
+		String  predName, content = null;
+		
+		XtextEditor editor = EditorUtils.getActiveXtextEditor(event);
+		if (editor == null) return null;
+		if (editor.isSaveOnCloseNeeded()) return null;
+		
+		IXtextDocument document = XtextDocumentUtil.get(editor);
+		IResource resource = editor.getResource();
+		
+		int offset = editor.getHighlightRange().getOffset();
+		int line = getLine(document, offset) + 1;
+		if (line == -1) return null;
+		
 		try {
 			content = editor.getDocument().get(offset, editor.getDocument().getLineLength(line));
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
-
-		StringTokenizer tmp = new StringTokenizer(content, " [{");
 		
+		StringTokenizer tmp = new StringTokenizer(content, " [{");
 		predName = tmp.nextToken();
 		while ("pred".equals(predName) || "private".equals(predName)) {
 			predName = tmp.nextToken();
 		}
-		if(predName.contains(".")) predName=predName.substring(predName.indexOf(".")+1);
-		file = new ALSFile(resource);
-		String filename = file.getFilename();
-		IReporter reporter = new Reporter(resource);
-		world = getWorld(reporter, filename);
-		if (world == null)
-			return null;
-		IPreferenceStore store = AlsActivator.getInstance().getPreferenceStore();
-		int scope=Integer.parseInt(store.getString(PreferenceConstants.DEFAULT_LAUNCH_OPTION));
-		try {
-			Func f=findPredicate(world, predName);
-			if (f.decls.isEmpty()) {
-				command = new Command(false, scope, -1, -1, f.call());
-			} else {
-				int lg=0;
-				for (Decl decl : f.decls) {					
-					lg+=decl.names.size();
-				}
-				Expr [] params = new Expr[lg];
-				int i=0;
-				for (Decl decl : f.decls) {
-					for(int j=0;j<decl.names.size();j++){
-						params[i++]=decl.expr;
-					}
-				}
-				command = new Command(false, scope, -1, -1, f.call(params));
-			}
-		} catch (Err e) {
-			e.printStackTrace();
-		}
-		
-		String cmd1="Run "+ predName ;
-		ExecutableCommand ex = new ExecutableCommand(file, command, 0, world, cmd1, scope);
-		// DebugUITools.launch(LaunchQuickConfigFactory.getInstance().create(ex, reporter), LaunchConfigurationConstants.RUN_MODE);
+		if(predName.contains(".")) 
+			predName = predName.substring(predName.indexOf(".") + 1);
+				
+		ALSImageRegistry.getImageRegistry();
+		DebugUITools.launch(LaunchQuickConfigFactory.getInstance().createPredicate(resource, predName), LaunchConfigurationConstants.RUN_MODE);
 		return null;
 	}
 
@@ -113,35 +60,5 @@ public class PredicateOutlineNodeHandler extends AbstractHandler {
 			return -1;
 		}
 		return line;
-	}
-
-	private CompModule getWorld(IReporter reporter, String filename) {
-		CompModule world;
-		try {
-			world = CompUtil.parseEverything_fromFile(reporter, null, filename);
-		} catch (Err e) {
-			return null;
-		}
-		return world;
-	}
-	
-	public Func findPredicate(Module world,String predicate){
-		SafeList<Func> l=world.getAllFunc();
-		for(Func c:l){
-			if(c.toString().contains(predicate)){
-				return c;
-			}
-		}
-		return null;
-	}
-	public Pair<String,Expr> findAssertion(Module world,String assertion){
-		ConstList<Pair<String,Expr>> l=world.getAllAssertions();
-		for(Pair<String,Expr> c:l){
-			if(c.a.equals(assertion)){
-				return c;
-			}
-		}
-		
-		return null;
 	}
 }
