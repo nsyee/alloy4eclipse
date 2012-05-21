f78f34ba9fed63f61117ffa241c9a5bf0328d99e
hetman.freddy <hetman.freddy@751a51b9-0729-0410-b983-630554dc95c4>
2012-05-21 16:36:23 +0000

    delegation work ! error corrected by cast...

--- trunk/fr.univartois.cril.xtext2.ui/src/fr/univartois/cril/xtext2/ui/editor/outline/CommandOutlineNodeHandler.java
+++ trunk/fr.univartois.cril.xtext2.ui/src/fr/univartois/cril/xtext2/ui/editor/outline/CommandOutlineNodeHandler.java
@@ -5,7 +5,10 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.editor.model.XtextDocumentUtil;
 import org.eclipse.xtext.ui.editor.utils.EditorUtils;
 
 import fr.univartois.cril.xtext2.alloyplugin.api.ALSImageRegistry;
@@ -19,10 +22,25 @@ public class CommandOutlineNodeHandler extends AbstractHandler {
 		if (editor == null) return null;
 		if (editor.isSaveOnCloseNeeded()) return null;
 		
+		IXtextDocument document = XtextDocumentUtil.get(editor);
+    	int offset = editor.getHighlightRange().getOffset();
+		int line = getLine(document, offset) + 1;
+		if (line == -1) return null;
+		
 		IResource resource = editor.getResource();
         
 		ALSImageRegistry.getImageRegistry();
-		DebugUITools.launch(LaunchQuickConfigFactory.getInstance().createCommand(resource, ""), LaunchConfigurationConstants.RUN_MODE);
+		DebugUITools.launch(LaunchQuickConfigFactory.getInstance().createCommand(resource, "" + line), LaunchConfigurationConstants.RUN_MODE);
 		return null;
 	}
+	
+	private int getLine(IXtextDocument document, int offset) {
+        int line = -1;
+        try {
+			line = document.getLineOfOffset(offset);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+        return line;
+    }
 }
